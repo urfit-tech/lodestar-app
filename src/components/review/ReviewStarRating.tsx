39ac603b9355a412b8a8db6076d1c0a6d514f664
ponchimeow <trancetoradora@gmail.com>
2020-12-07 20:02:41 +0800

    [BUGFIX] add half star, and add half star display

--- src/components/review/ReviewStarRating.tsx
+++ src/components/review/ReviewStarRating.tsx
@@ -1,20 +1,32 @@
 import { Icon } from '@chakra-ui/react'
 import React from 'react'
+import styled from 'styled-components'
 import { v4 as uuid } from 'uuid'
 import { ReactComponent as StarGrayIcon } from '../../images/star-gray.svg'
+import { ReactComponent as StarHalfIcon } from '../../images/star-half.svg'
 import { ReactComponent as StarIcon } from '../../images/star.svg'
+
+const StyledStarRating = styled.div`
+  && svg {
+    margin-right: 2px;
+  }
+  svg:last-child {
+    margin-right: 4px;
+  }
+`
 
 const ReviewStarRating: React.FC<{ score: number; boxSize?: string }> = ({ score, boxSize }) => {
   let starLists = []
-  for (let i = 0; i < score; i++) {
-    starLists.push(<Icon key={uuid()} boxSize={boxSize} style={{ marginRight: '2px' }} as={StarIcon} />)
+  for (let i = 0; i < Math.floor(score); i++) {
+    starLists.push(<Icon key={uuid()} boxSize={boxSize} as={StarIcon} />)
   }
+  if (score - Math.floor(score) > 0) starLists.push(<Icon key={uuid()} boxSize={boxSize} as={StarHalfIcon} />)
   if (starLists.length < 5) {
     for (let i = starLists.length; i < 5; i++) {
-      starLists.push(<Icon key={uuid()} boxSize={boxSize} style={{ marginRight: '2px' }} as={StarGrayIcon} />)
+      starLists.push(<Icon key={uuid()} boxSize={boxSize} as={StarGrayIcon} />)
     }
   }
-  return <div className="d-flex">{starLists}</div>
+  return <StyledStarRating className="d-flex">{starLists}</StyledStarRating>
 }
 
 export default ReviewStarRating
