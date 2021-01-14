e7896da7fa96b09770dfd035dca20b8c8889d7e2
Elantris <j88107333@gmail.com>
2021-01-14 16:10:46 +0800

    [BUGFIX] fix typegen

--- src/components/issue/IssueReplyCollectionBlock.tsx
+++ src/components/issue/IssueReplyCollectionBlock.tsx
@@ -1,9 +1,7 @@
-import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
 import React from 'react'
 import { useIntl } from 'react-intl'
 import { issueMessages } from '../../helpers/translation'
-import types from '../../types'
+import { useIssueReply } from '../../hooks/issue'
 import { ProgramRoleProps } from '../../types/program'
 import { useAuth } from '../auth/AuthContext'
 import IssueReplyCreationBlock from './IssueReplyCreationBlock'
@@ -15,54 +13,39 @@ const IssueReplyCollectionBlock: React.FC<{
 }> = ({ programRoles, issueId }) => {
   const { formatMessage } = useIntl()
   const { currentMemberId } = useAuth()
-  const { loading, data, error, refetch } = useQuery<types.GET_ISSUE_REPLIES, types.GET_ISSUE_REPLIESVariables>(
-    GET_ISSUE_REPLIES,
-    { variables: { issueId } },
-  )
+  const { loadingIssueReplies, errorIssueReplies, issueReplies, refetchIssueReplies } = useIssueReply(issueId)
 
   return (
     <>
-      {loading
+      {loadingIssueReplies
         ? formatMessage(issueMessages.status.loadingReply)
-        : error || !data
+        : errorIssueReplies
         ? formatMessage(issueMessages.status.loadingReplyError)
-        : data.issue_reply.map((value: any) => (
-            <div key={value.id} className="mt-5">
+        : issueReplies.map(issueReply => (
+            <div key={issueReply.id} className="mt-5">
               <IssueReplyItem
                 programRoles={programRoles}
-                issueReplyId={value.id}
-                memberId={value.member_id}
-                content={value.content}
-                createdAt={new Date(value.created_at)}
-                reactedMemberIds={value.issue_reply_reactions.map((value: any) => value.public_member.id)}
-                onRefetch={() => refetch()}
+                issueReplyId={issueReply.id}
+                memberId={issueReply.memberId}
+                content={issueReply.content}
+                createdAt={new Date(issueReply.createdAt)}
+                reactedMemberIds={issueReply.reactedMemberIds}
+                onRefetch={() => refetchIssueReplies()}
               />
             </div>
           ))}
+
       {currentMemberId && (
         <div className="mt-5">
-          <IssueReplyCreationBlock memberId={currentMemberId} issueId={issueId} onRefetch={() => refetch()} />
+          <IssueReplyCreationBlock
+            memberId={currentMemberId}
+            issueId={issueId}
+            onRefetch={() => refetchIssueReplies()}
+          />
         </div>
       )}
     </>
   )
 }
-
-const GET_ISSUE_REPLIES = gql`
-  query GET_ISSUE_REPLIES($issueId: uuid!) {
-    issue_reply(where: { issue_id: { _eq: $issueId } }, order_by: [{ created_at: asc }]) {
-      id
-      content
-      created_at
-      member_id
-      issue_reply_reactions {
-        public_member {
-          id
-          name
-        }
-      }
-    }
-  }
-`
 
 export default IssueReplyCollectionBlock
