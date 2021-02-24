89de882232ba1db0c9ed946b72123a005f0c025a
ponchimeow <trancetoradora@gmail.com>
2021-02-24 10:25:21 +0800

    [REFACTOR] rename modular page

--- src/pages/ProjectPage/ModularPage.tsx
+++ src/pages/ProjectPage/ModularPage.tsx
@@ -21,7 +21,7 @@ import { useProject } from '../../hooks/project'
 import EmptyCover from '../../images/empty-cover.png'
 import LoadingPage from '../LoadingPage'
 
-const ProjectModularPage: React.FC<{ projectId: string }> = ({ projectId }) => {
+const ModularPage: React.FC<{ projectId: string }> = ({ projectId }) => {
   const { settings } = useApp()
   const { project } = useProject(projectId)
   const [displaySectionTypes, setDisplaySectionTypes] = useState<string[]>(
@@ -199,4 +199,4 @@ const ProjectModularPage: React.FC<{ projectId: string }> = ({ projectId }) => {
   )
 }
 
-export default ProjectModularPage
+export default ModularPage
