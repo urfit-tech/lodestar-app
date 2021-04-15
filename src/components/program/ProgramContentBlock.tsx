d4586424cb9e9dfa0fe44bc376d57b3ed4282f98
FT <ftshen@qraft.app>
2021-04-15 12:08:08 +0800

    [BUGFIX] recover program-content-log

--- src/components/program/ProgramContentBlock.tsx
+++ src/components/program/ProgramContentBlock.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/react-hooks'
-import { Skeleton, Tabs } from 'antd'
+import { message, Skeleton, Tabs } from 'antd'
+import axios from 'axios'
 import BraftEditor from 'braft-editor'
 import gql from 'graphql-tag'
 import { flatten, includes } from 'ramda'
@@ -93,25 +94,25 @@ const ProgramContentBlock: React.FC<{
               lastProgress: played,
             }).then(() => refetchProgress())
           }}
-          // onEventTrigger={data => {
-          //   axios
-          //     .post(
-          //       `https://${apiHost}/tasks/player-event-logs/`,
-          //       {
-          //         programContentId,
-          //         data,
-          //       },
-          //       { headers: { authorization: `Bearer ${authToken}` } },
-          //     )
-          //     .then(({ data: { code, result } }) => {
-          //       if (code === 'SUCCESS') {
-          //         // process.env.NODE_ENV === 'development' && console.log({ result })
-          //         return
-          //       }
-          //       return message.error(formatMessage(codeMessages[code as keyof typeof codeMessages]))
-          //     })
-          //     .catch(handleError)
-          // }}
+          onEventTrigger={data => {
+            axios
+              .post(
+                `https://${apiHost}/tasks/player-event-logs/`,
+                {
+                  programContentId,
+                  data,
+                },
+                { headers: { authorization: `Bearer ${authToken}` } },
+              )
+              .then(({ data: { code, result } }) => {
+                if (code === 'SUCCESS') {
+                  // process.env.NODE_ENV === 'development' && console.log({ result })
+                  return
+                }
+                return message.error(formatMessage(codeMessages[code as keyof typeof codeMessages]))
+              })
+              .catch(handleError)
+          }}
           onEnded={() => {
             setTimeout(() => {
               insertProgress(programContentId, {
