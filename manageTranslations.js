1034ac43f93a4447ed1284b2e4462834bacbcb34
Elantris <j88107333@gmail.com>
2021-05-03 14:59:56 +0800

    [FEATURE] add zh-cn translation

--- manageTranslations.js
+++ manageTranslations.js
@@ -9,5 +9,5 @@ manageTranslations({
   messagesDirectory: 'build/messages/src/extracted/',
   translationsDirectory: 'src/translations/locales/',
   whitelistsDirectory: 'src/translations/locales/whitelists/',
-  languages: ['en', 'vi'],
+  languages: ['en', 'vi', 'zh-cn'],
 })
--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -6,3 +6,23 @@ export const CommonTextMixin = css`
   letter-spacing: 0.4px;
   color: var(--gray-dark);
 `
+export const CommonLargeTextMixin = css`
+  font-size: 16px;
+  letter-spacing: 0.2px;
+  font-weight: 500;
+  color: var(--gray-darker);
+`
+
+export const CommonTitleMixin = css`
+  font-size: 18px;
+  letter-spacing: 0.8px;
+  font-weight: bold;
+  color: var(--gray-darker);
+`
+
+export const CommonLargeTitleMixin = css`
+  font-size: 24px;
+  letter-spacing: 0.2px;
+  font-weight: bold;
+  color: var(--gray-darker);
+`
