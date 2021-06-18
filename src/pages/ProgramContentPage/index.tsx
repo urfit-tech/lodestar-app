20aabe5403f12e462fb92692484a95b824f9df7b
Elantris <j88107333@gmail.com>
2021-06-18 15:40:14 +0800

    [FEATURE] extract common styled components in program content page

--- src/pages/ProgramContentPage/index.tsx
+++ src/pages/ProgramContentPage/index.tsx
@@ -1,9 +1,8 @@
-import { Button, Icon, Layout, PageHeader } from 'antd'
+import { Button, Icon, Layout } from 'antd'
 import React, { useState } from 'react'
 import { BsStar } from 'react-icons/bs'
 import { useIntl } from 'react-intl'
 import { useHistory, useLocation, useParams } from 'react-router-dom'
-import styled from 'styled-components'
 import { useAuth } from '../../components/auth/AuthContext'
 import { BREAK_POINT } from '../../components/common/Responsive'
 import { StyledLayoutContent } from '../../components/layout/DefaultLayout.styled'
@@ -12,46 +11,8 @@ import { useApp } from '../../containers/common/AppContext'
 import { ProgressProvider } from '../../contexts/ProgressContext'
 import { commonMessages } from '../../helpers/translation'
 import { useProgram } from '../../hooks/program'
+import { StyledPageHeader, StyledSideBar } from './index.styled'
 import ProgramContentBlock from './ProgramContentBlock'
-
-const StyledPageHeader = styled(PageHeader)`
-  && {
-    padding: 1rem 1.5rem;
-    height: 4rem;
-    background: white;
-  }
-
-  .ant-page-header-heading {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .ant-page-header-heading-title,
-  .ant-divider {
-    display: none;
-  }
-
-  .ant-page-header-heading-extra {
-    width: auto;
-    padding: 0;
-  }
-
-  @media (min-width: ${BREAK_POINT}px) {
-    .ant-page-header-heading-title {
-      display: block;
-      flex-grow: 1;
-      font-size: 16px;
-      line-height: 32px;
-    }
-  }
-`
-const StyledSideBar = styled.div`
-  height: calc(100vh - 64px);
-  overflow-y: auto;
-  background: white;
-  box-shadow: rgba(0, 0, 0, 0.1) -3px 10px 10px 0px;
-`
 
 const ProgramContentPage: React.VFC = () => {
   const { formatMessage } = useIntl()
