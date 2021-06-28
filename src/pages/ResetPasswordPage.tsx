fe72ddb60589649e06d20f76d171d0f0c4d6b794
emily40830 <emily40830@gmail.com>
2021-06-28 18:07:30 +0800

    [FEATURE] update request body for POST /auth/reset-password

--- src/pages/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage.tsx
@@ -9,6 +9,7 @@ import { StringParam, useQueryParam } from 'use-query-params'
 import { useAuth } from '../components/auth/AuthContext'
 import { BREAK_POINT } from '../components/common/Responsive'
 import DefaultLayout from '../components/layout/DefaultLayout'
+import { useApp } from '../containers/common/AppContext'
 import { handleError } from '../helpers'
 import { codeMessages, commonMessages, usersMessages } from '../helpers/translation'
 
@@ -38,7 +39,9 @@ const ResetPasswordPage: React.VFC<FormComponentProps> = ({ form }) => {
   const { formatMessage } = useIntl()
   const history = useHistory()
   const [token] = useQueryParam('token', StringParam)
+  const [memberId] = useQueryParam('member', StringParam)
   const { apiHost } = useAuth()
+  const { id: appId } = useApp()
   const [loading, setLoading] = useState(false)
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -47,13 +50,12 @@ const ResetPasswordPage: React.VFC<FormComponentProps> = ({ form }) => {
       if (!error) {
         setLoading(true)
         axios
-          .post(
-            `https://${apiHost}/auth/reset-password`,
-            { newPassword: values.password, resetPasswordToken: token },
-            {
-              headers: { authorization: `Bearer ${token}` },
-            },
-          )
+          .post(`https://${apiHost}/auth/reset-password`, {
+            appId,
+            memberId,
+            newPassword: values.password,
+            resetPasswordToken: token,
+          })
           .then(({ data: { code } }) => {
             if (code === 'SUCCESS') {
               history.push('/reset-password-success')
