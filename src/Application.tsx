fba32e2ce1ccef4bb9d02ec82276f548ad863c06
emily40830 <emily40830@gmail.com>
2021-07-19 22:29:53 +0800

    [BUGFIX] remove localhost test

--- src/Application.tsx
+++ src/Application.tsx
@@ -26,7 +26,6 @@ const Application: React.FC<{
   customRender?: CustomRendererProps
 }> = ({ appId, extraRouteProps, customRender }) => {
   const apiHost = useApiHost(appId)
-
   if (!apiHost) {
     return <LoadingPage />
   }
