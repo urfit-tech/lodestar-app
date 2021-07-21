af1c58a0dbd7626fecaad843f107f14c5c433dd0
sunnylochun <lo@qraft.app>
2021-07-21 21:30:38 +0800

    [FEATURE] add custom renderer to admin menu

--- src/components/common/AdminMenu.tsx
+++ src/components/common/AdminMenu.tsx
@@ -8,6 +8,7 @@ import { useIntl } from 'react-intl'
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import { useApp } from '../../containers/common/AppContext'
+import { useCustomRenderer } from '../../contexts/CustomRendererContext'
 import hasura from '../../hasura'
 import { commonMessages } from '../../helpers/translation'
 import { useEnrolledMembershipCardIds } from '../../hooks/card'
@@ -34,8 +35,12 @@ const StyledMenu = styled(Menu)`
     background: none !important;
   }
 `
+export type renderMemberAdminMenuProps = {
+  menuProps: MenuProps
+  defaultMenuItems: { key: string; item: React.ReactElement | boolean | undefined }[]
+}
 
-const AdminMenu: React.FC<MenuProps> = ({ children, ...menuProps }) => {
+export const AdminMenu: React.FC<MenuProps> = ({ children, ...menuProps }) => {
   const { id: appId } = useApp()
   const { formatMessage } = useIntl()
   const history = useHistory()
@@ -62,90 +67,143 @@ const AdminMenu: React.FC<MenuProps> = ({ children, ...menuProps }) => {
 
 export const MemberAdminMenu: React.VFC<MenuProps> = ({ ...props }) => {
   const { formatMessage } = useIntl()
+  const { renderMemberAdminMenu } = useCustomRenderer()
   const { currentMemberId, currentUserRole } = useAuth()
   const { enabledModules, settings } = useApp()
   const { enrolledMembershipCardIds } = useEnrolledMembershipCardIds(currentMemberId || '')
   const { socialCards } = useSocialCardCollection()
 
-  return (
-    <AdminMenu {...props} style={{ background: 'transparent', border: 'none' }}>
-      {currentUserRole === 'content-creator' && (
+  const defaultMenuItems: renderMemberAdminMenuProps['defaultMenuItems'] = [
+    {
+      key: 'creator_management_system',
+      item: currentUserRole === 'content-creator' && (
         <Menu.Item key="creator_management_system" className="creatorManagementSystem">
           <SettingsIcon className="mr-2" />
           {formatMessage(commonMessages.content.creatorManagementSystem)}
         </Menu.Item>
-      )}
-      <Menu.Item key="member_profile_admin">
-        <Icon as={UserIcon} className="mr-2" />
-        {formatMessage(commonMessages.content.personalSettings)}
-      </Menu.Item>
-      <Menu.Item key="member_program_issues_admin">
-        <Icon as={BookIcon} className="mr-2" />
-        {formatMessage(commonMessages.content.courseProblem)}
-      </Menu.Item>
-      {enabledModules.practice && (
+      ),
+    },
+    {
+      key: 'member_profile_admin',
+      item: (
+        <Menu.Item key="member_profile_admin">
+          <Icon as={UserIcon} className="mr-2" />
+          {formatMessage(commonMessages.content.personalSettings)}
+        </Menu.Item>
+      ),
+    },
+    {
+      key: 'member_program_issues_admin',
+      item: (
+        <Menu.Item key="member_program_issues_admin">
+          <Icon as={BookIcon} className="mr-2" />
+          {formatMessage(commonMessages.content.courseProblem)}
+        </Menu.Item>
+      ),
+    },
+    {
+      key: 'member_practices_admin',
+      item: enabledModules.practice && (
         <Menu.Item key="member_practices_admin">
           <Icon as={BookIcon} className="mr-2" />
           {formatMessage(commonMessages.content.practiceManagement)}
         </Menu.Item>
-      )}
-      <Menu.Item key="member_orders_admin">
-        <Icon as={ClipboardListIcon} className="mr-2" />
-        {formatMessage(commonMessages.content.orderHistory)}
-      </Menu.Item>
-
-      {enabledModules.contract && (
+      ),
+    },
+    {
+      key: 'member_orders_admin',
+      item: (
+        <Menu.Item key="member_orders_admin">
+          <Icon as={ClipboardListIcon} className="mr-2" />
+          {formatMessage(commonMessages.content.orderHistory)}
+        </Menu.Item>
+      ),
+    },
+    {
+      key: 'member_contracts_admin',
+      item: enabledModules.contract && (
         <Menu.Item key="member_contracts_admin">
           <Icon as={ClipboardListIcon} className="mr-2" />
           {formatMessage(commonMessages.content.contracts)}
         </Menu.Item>
-      )}
-
-      {enabledModules.coin && (
+      ),
+    },
+    {
+      key: 'member_coins_admin',
+      item: enabledModules.coin && (
         <Menu.Item key="member_coins_admin">
           <Icon as={CoinIcon} className="mr-2" />
           {formatMessage(commonMessages.content.coinsAdmin)}
         </Menu.Item>
-      )}
-
-      {enabledModules.social_connect && socialCards.length && (
+      ),
+    },
+    {
+      key: 'member_social_cards',
+      item: enabledModules.social_connect && !!socialCards.length && (
         <Menu.Item key="member_social_cards">
           <Icon as={IdentityIcon} className="mr-2" />
           {formatMessage(commonMessages.content.socialCard)}
         </Menu.Item>
-      )}
-
-      {enabledModules.group_buying && (
+      ),
+    },
+    {
+      key: 'member_group_buying_admin',
+      item: enabledModules.group_buying && (
         <Menu.Item key="member_group_buying_admin">
           <Icon as={GroupBuyIcon} className="mr-2" />
           {formatMessage(commonMessages.ui.groupBuying)}
         </Menu.Item>
-      )}
-
-      <Menu.Item key="member_coupons_admin">
-        <Icon as={TicketIcon} className="mr-2" />
-        {formatMessage(commonMessages.content.coupon)}
-      </Menu.Item>
-
-      {enabledModules.voucher && (
+      ),
+    },
+    {
+      key: 'member_coupons_admin',
+      item: (
+        <Menu.Item key="member_coupons_admin">
+          <Icon as={TicketIcon} className="mr-2" />
+          {formatMessage(commonMessages.content.coupon)}
+        </Menu.Item>
+      ),
+    },
+    {
+      key: 'member_voucher_admin',
+      item: enabledModules.voucher && (
         <Menu.Item key="member_voucher_admin">
           <Icon as={GiftIcon} className="mr-2" />
           {formatMessage(commonMessages.content.voucher)}
         </Menu.Item>
-      )}
-
-      {enabledModules.member_card && enrolledMembershipCardIds.length > 0 && (
+      ),
+    },
+    {
+      key: 'member_cards_admin',
+      item: enabledModules.member_card && enrolledMembershipCardIds.length > 0 && (
         <Menu.Item key="member_cards_admin">
           <Icon as={MemberCardIcon} className="mr-2" />
           {formatMessage(commonMessages.content.memberCard)}
         </Menu.Item>
-      )}
+      ),
+    },
+    {
+      key: 'customer_support_link',
+      item: (
+        <Menu.Item key="_blank" data-href={settings['customer_support_link']}>
+          <Icon as={CommentsIcon} className="mr-2" />
+          {formatMessage(commonMessages.content.contact)}
+        </Menu.Item>
+      ),
+    },
+  ]
 
-      <Menu.Item key="_blank" data-href={settings['customer_support_link']}>
-        <Icon as={CommentsIcon} className="mr-2" />
-        {formatMessage(commonMessages.content.contact)}
-      </Menu.Item>
-    </AdminMenu>
+  return (
+    <>
+      {renderMemberAdminMenu?.({
+        menuProps: props,
+        defaultMenuItems,
+      }) || (
+        <AdminMenu {...props} style={{ background: 'transparent', border: 'none' }}>
+          {defaultMenuItems.map(v => v.item)}
+        </AdminMenu>
+      )}
+    </>
   )
 }
 
