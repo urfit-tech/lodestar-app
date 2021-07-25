31e292c31861fe964ebcb19dc8f8b9ddcea435fb
徐愷 <kkshyu.tw@gmail.com>
2021-07-25 21:30:41 +0800

    [BUGFIX] display error if app cannot be loaded

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,9 @@ import { unregister } from './serviceWorker'
 const appId = process.env.REACT_APP_ID || (window as any).APP_ID
 const Application = process.env.NODE_ENV === 'production' ? App : hot(App)
 const rootElement = document.getElementById('root')
-if (rootElement && rootElement.hasChildNodes()) {
+if (!appId) {
+  render(<div>Application cannot be loaded.</div>, rootElement)
+} else if (rootElement && rootElement.hasChildNodes()) {
   hydrate(<Application appId={appId} />, rootElement)
 } else {
   render(<Application appId={appId} />, rootElement)
