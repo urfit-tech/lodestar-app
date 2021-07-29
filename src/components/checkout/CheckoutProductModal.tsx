8fdf9c6e91f18b44aff7f683bd6096036c8531a0
FT <ftshen@qraft.app>
2021-07-29 12:25:42 +0800

    [HOTFIX] should select payment if type of payment is perpetual

--- src/components/checkout/CheckoutProductModal.tsx
+++ src/components/checkout/CheckoutProductModal.tsx
@@ -139,7 +139,9 @@ const CheckoutProductModal: React.VFC<CheckoutProductModalProps> = ({
 
   const [shipping, setShipping] = useState<ShippingProps>(cachedPaymentInfor.shipping)
   const [invoice, setInvoice] = useState<InvoiceProps>(cachedPaymentInfor.invoice)
-  const [payment, setPayment] = useState<PaymentProps>(cachedPaymentInfor.payment)
+  const [payment, setPayment] = useState<PaymentProps | null>(
+    paymentType === 'perpetual' ? null : { gateway: 'tappay', method: 'credit' },
+  )
   const [isValidating, setIsValidating] = useState(false)
   const [discountId, setDiscountId] = useState('')
   const [referrerEmail, setReferrerEmail] = useState('')
@@ -176,6 +178,10 @@ const CheckoutProductModal: React.VFC<CheckoutProductModalProps> = ({
     const isValidShipping = !isProductPhysical || validateShipping(shipping)
     const isValidInvoice = validateInvoice(invoice).length === 0
 
+    if (!payment) {
+      paymentMethodRef.current?.scrollIntoView({ behavior: 'smooth' })
+      return
+    }
     if (!isValidShipping) {
       shippingRef.current?.scrollIntoView({ behavior: 'smooth' })
       return
