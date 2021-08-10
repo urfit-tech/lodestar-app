6b705f504c5dcb666fceeecfe271c8299ccd0b99
ponchimeow <trancetoradora@gmail.com>
2021-08-10 11:56:14 +0800

    [REFACTOR] combine UseRadioProps, change React.VFC to React.FC

--- src/components/RadioCard.tsx
+++ src/components/RadioCard.tsx
@@ -1,11 +1,13 @@
 import { Box, useRadio, UseRadioProps, useTheme } from '@chakra-ui/react'
 
-const RadioCard: React.VFC<{
-  size?: 'xs' | 'sm' | 'md' | 'lg'
-  children: React.ReactNode
-}> = props => {
+const RadioCard: React.FC<
+  {
+    size?: 'xs' | 'sm' | 'md' | 'lg'
+    children: React.ReactNode
+  } & UseRadioProps
+> = ({ size, children, ...props }) => {
   const theme = useTheme()
-  const { getInputProps, getCheckboxProps } = useRadio(props as UseRadioProps)
+  const { getInputProps, getCheckboxProps } = useRadio(props)
   const input = getInputProps()
   const checkbox = getCheckboxProps()
 
@@ -23,14 +25,12 @@ const RadioCard: React.VFC<{
           color: 'white',
           borderColor: `${theme.colors.primary[500]}`,
         }}
-        px={props.size === 'xs' ? 2 : props.size === 'sm' ? 3 : props.size === 'md' ? 4 : props.size === 'lg' ? 6 : 4}
-        h={props.size === 'xs' ? 6 : props.size === 'sm' ? 8 : props.size === 'md' ? 10 : props.size === 'lg' ? 12 : 10}
-        lineHeight={
-          props.size === 'xs' ? 6 : props.size === 'sm' ? 8 : props.size === 'md' ? 10 : props.size === 'lg' ? 12 : 10
-        }
+        px={size === 'xs' ? 2 : size === 'sm' ? 3 : size === 'md' ? 4 : size === 'lg' ? 6 : 4}
+        h={size === 'xs' ? 6 : size === 'sm' ? 8 : size === 'md' ? 10 : size === 'lg' ? 12 : 10}
+        lineHeight={size === 'xs' ? 6 : size === 'sm' ? 8 : size === 'md' ? 10 : size === 'lg' ? 12 : 10}
         textAlign="center"
       >
-        {props.children}
+        {children}
       </Box>
     </Box>
   )
