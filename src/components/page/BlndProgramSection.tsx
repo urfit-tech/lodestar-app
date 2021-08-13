2068362de5c5c16af01c9cdbc21809fce8301675
FT <ftshen@qraft.app>
2021-08-13 14:45:54 +0800

    [BUGFIX] fix naming & style

--- src/components/page/BlndProgramSection.tsx
+++ src/components/page/BlndProgramSection.tsx
@@ -102,7 +102,7 @@ const StyledLink = styled(Link)`
   background-color: #4c60ff;
 `
 
-const BlnbProgramSection: React.VFC<{
+const BlndProgramSection: React.VFC<{
   options: { backgroundImage: string; title: string; subtitle: string; quote: string; person: string }
 }> = ({ options: { backgroundImage, title, subtitle, quote, person } }) => {
   const { programs } = usePublishedProgramCollection({
@@ -113,7 +113,7 @@ const BlnbProgramSection: React.VFC<{
   return (
     <section className="row m-0">
       <StyledColumn className="col-12 col-lg-4" backgroundImage={backgroundImage}>
-        <StyledDecoration src="https://static.kolable.com/images/blnb/quot.svg" />
+        <StyledDecoration src="https://static.kolable.com/images/blnd/quot.svg" />
         <StyledTitle className="mb-2">{title}</StyledTitle>
         <StyledSubTitle className="mb-5">{subtitle}</StyledSubTitle>
         <StyledQuote className="mb-3">{quote}</StyledQuote>
@@ -151,4 +151,4 @@ const BlnbProgramSection: React.VFC<{
   )
 }
 
-export default BlnbProgramSection
+export default BlndProgramSection
--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -4,9 +4,9 @@ import styled from 'styled-components'
 import MessengerChat from '../components/common/MessengerChat'
 import DefaultLayout from '../components/layout/DefaultLayout'
 import ActivitySection from '../components/page/ActivitySection'
-import BlnbCoverSection from '../components/page/BlnbCoverSection'
-import BlnbPostSection from '../components/page/BlnbPostSection'
-import BlnbProgramSection from '../components/page/BlnbProgramSection'
+import BlndCoverSection from '../components/page/BlndCoverSection'
+import BlndPostSection from '../components/page/BlndPostSection'
+import BlndProgramSection from '../components/page/BlndProgramSection'
 import CoverSection from '../components/page/CoverSection'
 import CreatorSection from '../components/page/CreatorSection'
 import PostSection from '../components/page/PostSection'
@@ -55,9 +55,9 @@ const AppPage: React.VFC<{ page: AppPageProps }> = ({ page }) => {
     messenger: MessengerChat,
     homeStatic: StaticBlock,
     homeTeacher: TeacherSection,
-    homeBlnbCover: BlnbCoverSection,
-    homeBlnbProgram: BlnbProgramSection,
-    homeBlnbPost: BlnbPostSection,
+    homeBlndCover: BlndCoverSection,
+    homeBlndProgram: BlndProgramSection,
+    homeBlndPost: BlndPostSection,
   }
 
   return (
