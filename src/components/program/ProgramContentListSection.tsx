91532198365944cf116533132bee1fbffdda5e13
ponchimeow <trancetoradora@gmail.com>
2021-08-13 15:50:11 +0800

    [BUGFIX] fix subscription program display

--- src/components/program/ProgramContentListSection.tsx
+++ src/components/program/ProgramContentListSection.tsx
@@ -76,87 +76,93 @@ const ProgramContentListSection: React.VFC<{
         programContentSection.contents.filter(programContent => programContent.listPrice === 0) || [],
     ) || []
 
-  if (trialProgramContents.length === 0) {
-    return null
-  }
-
-  if (!program.isIntroductionSectionVisible) {
-    // subscription program
+  const ProgramContentSectionBlock = () => {
     return (
       <>
-        <StyledTitle>{formatMessage(productMessages.program.title.trial)}</StyledTitle>
+        <StyledTitle>{formatMessage(productMessages.program.title.content)}</StyledTitle>
         <Divider className="mt-1" />
 
-        {trialProgramContents.map(programContent => {
-          return (
-            <ProgramContentTrialModal
-              key={programContent.id}
-              programContentId={programContent.id}
-              render={({ setVisible }) => (
-                <ProgramContentItem onClick={() => setVisible(true)}>
+        {program.contentSections
+          .filter(programContentSection => programContentSection.contents.length)
+          .map(programContentSection => (
+            <ProgramSectionBlock key={programContentSection.id}>
+              <ProgramSectionTitle className="mb-3">{programContentSection.title}</ProgramSectionTitle>
+
+              {programContentSection.contents.map(programContent => (
+                <ProgramContentItem key={programContent.id}>
                   <Typography.Text>
-                    {programContent.duration ? (
+                    {programContent.contentType === 'video' ? (
                       <Icon type="video-camera" className="mr-2" />
                     ) : (
                       <Icon type="file-text" className="mr-2" />
                     )}
-                    {programContent.title}
+                    <span>{programContent.title}</span>
                   </Typography.Text>
 
-                  <StyledDuration>{durationFormatter(programContent.duration) || ''}</StyledDuration>
+                  <StyledDuration>
+                    {programContent.listPrice === 0 && !isEnrolled && (
+                      <ProgramContentTrialModal
+                        programContentId={programContent.id}
+                        render={({ setVisible }) => (
+                          <StyledObscure onClick={() => setVisible(true)}>
+                            <StyledTag color={theme['@primary-color']}>
+                              {formatMessage(productMessages.program.content.trial)}
+                            </StyledTag>
+                          </StyledObscure>
+                        )}
+                      />
+                    )}
+                    {durationFormatter(programContent.duration) || ''}
+                  </StyledDuration>
                 </ProgramContentItem>
-              )}
-            />
-          )
-        })}
+              ))}
+            </ProgramSectionBlock>
+          ))}
       </>
     )
   }
 
-  // perpetual program
-  return (
-    <>
-      <StyledTitle>{formatMessage(productMessages.program.title.content)}</StyledTitle>
-      <Divider className="mt-1" />
+  if (program.isSubscription) {
+    // subscription program
+    if (!program.isIntroductionSectionVisible) {
+      if (trialProgramContents.length === 0) {
+        return null
+      }
+      return (
+        <>
+          <StyledTitle>{formatMessage(productMessages.program.title.trial)}</StyledTitle>
+          <Divider className="mt-1" />
 
-      {program.contentSections
-        .filter(programContentSection => programContentSection.contents.length)
-        .map(programContentSection => (
-          <ProgramSectionBlock key={programContentSection.id}>
-            <ProgramSectionTitle className="mb-3">{programContentSection.title}</ProgramSectionTitle>
-
-            {programContentSection.contents.map(programContent => (
-              <ProgramContentItem key={programContent.id}>
-                <Typography.Text>
-                  {programContent.contentType === 'video' ? (
-                    <Icon type="video-camera" className="mr-2" />
-                  ) : (
-                    <Icon type="file-text" className="mr-2" />
-                  )}
-                  <span>{programContent.title}</span>
-                </Typography.Text>
-
-                <StyledDuration>
-                  {programContent.listPrice === 0 && !isEnrolled && (
-                    <ProgramContentTrialModal
-                      programContentId={programContent.id}
-                      render={({ setVisible }) => (
-                        <StyledObscure onClick={() => setVisible(true)}>
-                          <StyledTag color={theme['@primary-color']}>
-                            {formatMessage(productMessages.program.content.trial)}
-                          </StyledTag>
-                        </StyledObscure>
+          {trialProgramContents.map(programContent => {
+            return (
+              <ProgramContentTrialModal
+                key={programContent.id}
+                programContentId={programContent.id}
+                render={({ setVisible }) => (
+                  <ProgramContentItem onClick={() => setVisible(true)}>
+                    <Typography.Text>
+                      {programContent.duration ? (
+                        <Icon type="video-camera" className="mr-2" />
+                      ) : (
+                        <Icon type="file-text" className="mr-2" />
                       )}
-                    />
-                  )}
-                  {durationFormatter(programContent.duration) || ''}
-                </StyledDuration>
-              </ProgramContentItem>
-            ))}
-          </ProgramSectionBlock>
-        ))}
-    </>
-  )
+                      {programContent.title}
+                    </Typography.Text>
+
+                    <StyledDuration>{durationFormatter(programContent.duration) || ''}</StyledDuration>
+                  </ProgramContentItem>
+                )}
+              />
+            )
+          })}
+        </>
+      )
+    }
+    return <ProgramContentSectionBlock />
+  }
+
+  // perpetual program
+  return <ProgramContentSectionBlock />
 }
 
 export default ProgramContentListSection
