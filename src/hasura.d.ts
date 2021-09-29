a0b025fd0854c792a5e959e75516bac7efc2fa1b
emily40830 <emily40830@gmail.com>
2021-09-29 15:29:26 +0800

    [FEATURE] add program author

--- src/hasura.d.ts
+++ src/hasura.d.ts
@@ -6625,6 +6625,12 @@ export interface GET_PUBLISHED_PROGRAM_COLLECTION_program_program_categories {
   category: GET_PUBLISHED_PROGRAM_COLLECTION_program_program_categories_category;
 }
 
+export interface GET_PUBLISHED_PROGRAM_COLLECTION_program_program_roles_member {
+  __typename: "member_public";
+  id: string | null;
+  name: string | null;
+}
+
 export interface GET_PUBLISHED_PROGRAM_COLLECTION_program_program_roles {
   __typename: "program_role";
   id: any;
@@ -6632,7 +6638,10 @@ export interface GET_PUBLISHED_PROGRAM_COLLECTION_program_program_roles {
    * instructor / assistant 
    */
   name: string;
-  member_id: string;
+  /**
+   * An object relationship
+   */
+  member: GET_PUBLISHED_PROGRAM_COLLECTION_program_program_roles_member | null;
 }
 
 export interface GET_PUBLISHED_PROGRAM_COLLECTION_program_program_plans_currency {
