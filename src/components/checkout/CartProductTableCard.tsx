625eb44f40a16e8fd6896e05fdc25a8023655610
emily40830 <emily40830@gmail.com>
2021-09-30 22:15:44 +0800

    [BUGFIX] move checkout event to cart product table card

--- src/components/checkout/CartProductTableCard.tsx
+++ src/components/checkout/CartProductTableCard.tsx
@@ -3,6 +3,7 @@ import { Button, Divider, Icon, SkeletonText } from '@chakra-ui/react'
 import { List } from 'antd'
 import { CardProps } from 'antd/lib/card'
 import gql from 'graphql-tag'
+import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 import React, { Fragment, useContext, useEffect } from 'react'
 import ReactGA from 'react-ga'
 import { AiOutlineClose } from 'react-icons/ai'
@@ -12,6 +13,7 @@ import CartContext from '../../contexts/CartContext'
 import hasura from '../../hasura'
 import { checkoutMessages } from '../../helpers/translation'
 import { useMemberShop } from '../../hooks/checkout'
+import { useProductCollection } from '../../hooks/common'
 import EmptyAvatar from '../../images/avatar.svg'
 import { CartProductProps } from '../../types/checkout'
 import AdminCard from '../common/AdminCard'
@@ -33,10 +35,45 @@ const CartProductTableCard: React.VFC<CartProductTableCardProps> = ({
   const { removeCartProducts } = useContext(CartContext)
   const { loading, cartProductsWithInventory: cartProducts, refetch } = useProductInventory(cartProductWithoutInventory)
   const { memberShop } = useMemberShop(shopId)
+  const getProductCollection = useProductCollection()
+  const { settings, id: appId } = useApp()
 
   useEffect(() => {
     refetch && refetch()
   })
+
+  // useEffect(() => {
+  if (settings['tracking.gtm_id'] && cartProducts) {
+    const cartProductIds = cartProducts.map(cartProduct => cartProduct.productId)
+
+    getProductCollection(cartProductIds)
+      .then(products => {
+        ;(window as any).dataLayer = (window as any).dataLayer || []
+        ;(window as any).dataLayer.push({ ecommerce: null })
+        ;(window as any).dataLayer.push({
+          event: 'checkout',
+          ecommerce: {
+            checkout: {
+              actionField: { step: 1, option: 'credit' },
+              products: products.map(product => ({
+                name: product.title,
+                id: product.id,
+                price: product.isOnSale ? product.salePrice : product.listPrice || 0,
+                brand: settings['title'] || appId,
+                categories: product.categories && product.categories.join('|'),
+                variant: product.roles && product.roles.join('|'),
+                quantity:
+                  cartProducts.find(cartProduct => cartProduct.productId.split('_')[1] === product.id)?.options
+                    ?.quantity || 1,
+              })),
+            },
+          },
+        })
+      })
+      .catch()
+  }
+  //}
+  //, [cartProducts])
 
   if (loading) {
     return (
