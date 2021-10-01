61ce38fdfe7a3490880873317770d43e8d260c03
emily40830 <emily40830@gmail.com>
2021-10-01 17:00:12 +0800

    [BUGFIX] fix property error

--- src/hasura.d.ts
+++ src/hasura.d.ts
@@ -8462,6 +8462,7 @@ export interface GET_ORDERS_PRODUCT_order_log_by_pk_order_discounts {
    */
   type: string;
   target: string;
+  name: string;
 }
 
 export interface GET_ORDERS_PRODUCT_order_log_by_pk {
