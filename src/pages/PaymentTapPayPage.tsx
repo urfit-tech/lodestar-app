19e264660471a6e4d78de4d761052a8386a7787c
ponchimeow <trancetoradora@gmail.com>
2021-10-14 12:12:47 +0800

    [BGUFIX] fix catch error

--- src/pages/PaymentTapPayPage.tsx
+++ src/pages/PaymentTapPayPage.tsx
@@ -85,7 +85,7 @@ const PaymentTapPayBlock: React.VFC = () => {
       }
       history.push(`/orders/${orderId}?tracking=1`)
     } catch (err) {
-      message.error(err)
+      if (err instanceof Error) message.error(err)
     }
     setIsPaying(false)
   }
