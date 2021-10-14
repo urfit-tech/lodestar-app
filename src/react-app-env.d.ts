f6f23b866bd9240ab1fe914376f6a6f00ae41683
徐愷 <kkshyu.tw@gmail.com>
2021-10-14 16:55:26 -0400

    [BUGFIX] podcast player with react-howler

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -10,3 +10,43 @@ declare module 'console' {
 
 type CustomFC<T = {}, V = {}> = React.FC<T & { render?: (props: V) => React.ReactElement }>
 type CustomVFC<T = {}, V = {}> = React.VFC<T & { render?: (props: V) => React.ReactElement }>
+
+declare module 'react-howler' {
+  declare enum HOWLER_STATE {
+    UNLOADED = 'unloaded',
+    LOADING = 'loading',
+    LOADED = 'loaded',
+  }
+
+  interface Props {
+    src: string | string[]
+    format?: string[] | undefined
+    playing?: boolean | undefined
+    mute?: boolean | undefined
+    loop?: boolean | undefined
+    preload?: boolean | undefined
+    volume?: number | undefined
+    rate?: number | undefined
+    onEnd?: (() => void) | undefined
+    onPause?: (() => void) | undefined
+    onPlay?: ((id: number) => void) | undefined
+    onVolume?: ((id: number) => void) | undefined
+    onStop?: ((id: number) => void) | undefined
+    onLoad?: (() => void) | undefined
+    onLoadError?: ((id: number) => void) | undefined
+    html5?: boolean | undefined
+  }
+
+  declare class ReactHowler extends React.Component<Props> {
+    stop(id?: number): void
+
+    duration(id?: number): number
+
+    seek(time?: number): number
+
+    howlerState(): HOWLER_STATE
+
+    howler: Howl
+  }
+  export default ReactHowler
+}
