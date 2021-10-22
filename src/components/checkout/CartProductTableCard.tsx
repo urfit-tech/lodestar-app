bf42bb68b6596766c461f5920708ef92fb9cb54f
sunnylochun <lo@qraft.app>
2021-10-22 14:52:17 +0800

    [FEATURE] add activity ticket buyable quantity query

--- src/components/checkout/CartProductTableCard.tsx
+++ src/components/checkout/CartProductTableCard.tsx
@@ -109,7 +109,7 @@ const CartProductTableCard: React.VFC<CartProductTableCardProps> = ({
         </>
       )}
 
-      <List itemLayout="horizontal" className="mb-4">
+      <List itemLayout="horizontal" className={cartProducts.length !== 0 ? 'mb-4' : ''}>
         {cartProducts.map(
           cartProduct =>
             cartProduct.productId && (
@@ -191,16 +191,27 @@ export const useProductInventory = (cartProducts: CartProductProps[]) => {
   >(GET_PRODUCT_INVENTORY, {
     variables: {
       productIds: cartProducts.map(cartProduct => cartProduct.productId),
+      activityTicketIds: cartProducts
+        .filter(cartProduct => cartProduct.productId.includes('ActivityTicket'))
+        .map(cartProduct => cartProduct.productId.split('_')[1]),
     },
   })
 
+  const activityTicketEnrollment =
+    data?.activity_ticket_enrollment_count?.map(v => ({
+      id: v.activity_ticket_id,
+      buyableQuantity: v.buyable_quantity,
+    })) || []
+
   const productInventories =
-    loading || error || !data
-      ? []
-      : data.product_inventory_status.map(productInventory => ({
-          productId: productInventory.product_id || '',
-          buyableQuantity: productInventory.buyable_quantity,
-        }))
+    data?.product_inventory_status.map(productInventory => ({
+      productId: productInventory.product_id || '',
+      buyableQuantity: productInventory.product_id?.includes('ActivityTicket')
+        ? activityTicketEnrollment.find(
+            activityTicket => activityTicket?.id === productInventory?.product_id?.split('_')[1],
+          )?.buyableQuantity
+        : productInventory.buyable_quantity,
+    })) || []
 
   return {
     loading,
