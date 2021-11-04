974f7a3e0b4e231be6aacccba5b19ac8c2ee85d3
徐愷 <kkshyu.tw@gmail.com>
2021-11-04 14:46:36 +0800

    [BUGFIX] slide style

--- src/components/page/CoverSection.tsx
+++ src/components/page/CoverSection.tsx
@@ -1,12 +1,7 @@
-import Carousel from 'lodestar-app-element/src/components/common/Carousel'
-import {
-  CraftButton,
-  CraftParagraph,
-  CraftSection,
-  CraftTitle,
-} from 'lodestar-app-element/src/components/common/CraftElement'
+import { CraftCarousel } from 'lodestar-app-element/src/components/common/CraftElement'
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { Slide } from './CustomCoverSection'
 
 const CoverSection: React.VFC<{
   options: {
@@ -20,54 +15,52 @@ const CoverSection: React.VFC<{
       link: string
       external: boolean
     }[]
-    sectionHeight: { desktopHeight: string; mobileHeight: string }
+    sectionHeight?: { desktopHeight?: string; mobileHeight?: string }
   }
 }> = ({ options }) => {
   const history = useHistory()
 
   return (
     <section>
-      <Carousel dots infinite arrows={false} autoplay autoplaySpeed={5000} variant="cover">
+      <CraftCarousel
+        dots
+        infinite
+        arrows={false}
+        autoplay
+        autoplaySpeed={5000}
+        variant="cover"
+        customStyle={{
+          height: options.sectionHeight?.mobileHeight,
+        }}
+        // FIXME: Carousel props
+        responsive={
+          {
+            desktop: {
+              customStyle: {
+                height: options.sectionHeight?.desktopHeight,
+              },
+            },
+          } as any
+        }
+      >
         {options.coverInfos.map(v => (
-          <CraftSection
-            responsive={{
-              desktop: {
-                customStyle: {
-                  backgroundImage: `url(${v.srcDesktop})`,
-                },
-              },
-              mobile: {
-                customStyle: {
-                  backgroundImage: `url(${v.srcMobile})`,
-                },
-              },
+          <Slide
+            srcDesktop={v.srcDesktop}
+            srcMobile={v.srcMobile}
+            title={v.title}
+            subtitle={v.subtitle}
+            buttonText={v.link && v.buttonText ? <Link to={v.link}>{v.buttonText}</Link> : undefined}
+            onClick={() => {
+              if (v.buttonText) {
+                return
+              }
+              if (!v.link) return
+              v.external ? window.open(v.link) : history.push(v.link)
             }}
-          >
-            <CraftTitle
-              title={v.title}
-              customStyle={{
-                fontSize: 40,
-                textAlign: 'center',
-                fontWeight: 'bold',
-                color: '#fff',
-                margin: 0,
-              }}
-            />
-            <CraftParagraph
-              content={v.subtitle}
-              customStyle={{
-                textAlign: 'center',
-                fontSize: 20,
-                fontWeight: 'normal',
-                lineHeight: 1.5,
-                color: '#fff',
-                margin: 0,
-              }}
-            />
-            <CraftButton title={v.buttonText} link={v.link} />
-          </CraftSection>
+            sectionHeight={options.sectionHeight}
+          />
         ))}
-      </Carousel>
+      </CraftCarousel>
     </section>
   )
 }
