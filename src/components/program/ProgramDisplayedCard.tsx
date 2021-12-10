261003cfa421ba745d2503f752da1371400bf482
FT <ftshen@qraft.app>
2021-12-10 10:45:05 +0800

    [BUGFIX] fix link router

--- src/components/program/ProgramDisplayedCard.tsx
+++ src/components/program/ProgramDisplayedCard.tsx
@@ -2,7 +2,6 @@ import { Typography } from 'antd'
 import { CommonTitleMixin } from 'lodestar-app-element/src/components/common'
 import { sum } from 'ramda'
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { useProgramContentProgress } from '../../contexts/ProgressContext'
 import EmptyCover from '../../images/empty-cover.png'
@@ -38,14 +37,12 @@ export const ProgramDisplayedCard: React.VFC<{
     : 0
 
   return (
-    <Link to={`/programs/${program.id}`}>
-      <div className="mb-4">
-        <StyledProgramCover className="mb-3" src={program.coverUrl || EmptyCover} />
-        <StyledProgramTitle level={2} ellipsis={{ rows: 2 }} className="mb-3">
-          {program.title}
-        </StyledProgramTitle>
-        {memberId && <ProgressBar percent={viewRate} />}
-      </div>
-    </Link>
+    <div className="mb-4">
+      <StyledProgramCover className="mb-3" src={program.coverUrl || EmptyCover} />
+      <StyledProgramTitle level={2} ellipsis={{ rows: 2 }} className="mb-3">
+        {program.title}
+      </StyledProgramTitle>
+      {memberId && <ProgressBar percent={viewRate} />}
+    </div>
   )
 }
