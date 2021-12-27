d1880bec34f5191649e9dfd39eb7e7c6c920ca42
sunnylochun <lo@qraft.app>
2021-12-27 14:07:19 +0800

    [FEATURE] add get cookie helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -347,3 +347,9 @@ export const useOnceAnimation = () => {
 
   return { ref, activated }
 }
+
+export const getCookie = (name: string) => {
+  const value = `; ${document.cookie}`
+  const parts = value.split(`; ${name}=`)
+  if (parts.length === 2) return parts.pop()?.split(';').shift()
+}
