be3205805a162e3610c9540250c82b6bbbc7759f
FT <ftshen@qraft.app>
2022-03-23 23:11:41 +0800

    [BUGFIX] fix category position

--- src/pages/AdvancedSearchPage.tsx
+++ src/pages/AdvancedSearchPage.tsx
@@ -165,7 +165,7 @@ const useSearchPrograms = (condition: hasura.GET_ADVANCE_SEARCH_PROGRAMSVariable
           id
           title
           cover_url
-          program_categories {
+          program_categories(order_by: { category: { position: asc } }) {
             id
             category {
               id
