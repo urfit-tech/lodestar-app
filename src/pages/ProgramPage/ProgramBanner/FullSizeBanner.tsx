c65011f2c11be089d5bc6c80c0f86528e21d5b23
徐愷 <kkshyu.tw@gmail.com>
2022-04-20 09:51:28 +0800

    [BUGFIX] adapt for mobile cover

--- src/pages/ProgramPage/ProgramBanner/FullSizeBanner.tsx
+++ src/pages/ProgramPage/ProgramBanner/FullSizeBanner.tsx
@@ -1,26 +1,22 @@
+import { BREAK_POINT } from 'lodestar-app-element/src/components/common/Responsive'
 import React from 'react'
 import styled from 'styled-components'
 
-const FullSizeContainer = styled.div<{ coverUrl?: string | null }>`
+const FullSizeContainer = styled.div<{ coverUrl?: { mobileUrl?: string; desktopUrl?: string } }>`
   width: 100vw;
   height: 41.68vw;
-  background-image: url(${props => props.coverUrl || ''});
+  background-image: url(${props => props.coverUrl?.mobileUrl || props.coverUrl?.desktopUrl});
   background-size: cover;
   background-position: center;
+  @media (min-width: ${BREAK_POINT}px) {
+    background-image: url(${props => props.coverUrl?.desktopUrl || props.coverUrl?.mobileUrl});
+  }
 `
 
 const FullSizeBanner: React.VFC<{
-  coverUrl?: string | null
+  coverUrl?: { mobileUrl?: string; desktopUrl?: string }
 }> = ({ coverUrl }) => {
-  return (
-    <>
-      {
-        coverUrl ?
-        <FullSizeContainer coverUrl={coverUrl} /> :
-        <></>
-      }
-    </>
-  )
+  return <>{coverUrl ? <FullSizeContainer coverUrl={coverUrl} /> : <></>}</>
 }
 
 export default FullSizeBanner
