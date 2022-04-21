2ccbf3dd032c2a1086fd4ccd0e8be564cde4384b
SasukeLiou <hongyiliou@cw.com.tw>
2022-04-21 11:03:29 +0800

    [dev] 調整手機版課程內頁banner高度

--- src/pages/ProgramPage/ProgramBanner/FullSizeBanner.tsx
+++ src/pages/ProgramPage/ProgramBanner/FullSizeBanner.tsx
@@ -4,11 +4,12 @@ import styled from 'styled-components'
 
 const FullSizeContainer = styled.div<{ coverUrl?: { mobileUrl?: string; desktopUrl?: string } }>`
   width: 100vw;
-  height: 41.68vw;
+  height: 104vw;
   background-image: url(${props => props.coverUrl?.mobileUrl || props.coverUrl?.desktopUrl});
   background-size: cover;
   background-position: center;
   @media (min-width: ${BREAK_POINT}px) {
+    height: 41.68vw;
     background-image: url(${props => props.coverUrl?.desktopUrl || props.coverUrl?.mobileUrl});
   }
 `
