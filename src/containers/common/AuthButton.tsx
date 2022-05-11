01501c1cdccd0c369f500ca61dc8d1f4cb7f2f20
SasukeLiou <hongyiliou@cw.com.tw>
2022-05-11 15:48:33 +0800

    [dev] 調整首頁登入前「免費加入天下學習」按鈕連結

--- src/containers/common/AuthButton.tsx
+++ src/containers/common/AuthButton.tsx
@@ -1,12 +1,14 @@
 import { Button, Popover } from 'antd'
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useIntl } from 'react-intl'
 import { AuthModalContext } from '../../components/auth/AuthModal'
 import { CustomNavLinks, StyledList, Wrapper } from '../../components/common/MemberProfileButton'
 import Responsive from '../../components/common/Responsive'
 import { useCustomRenderer } from '../../contexts/CustomRendererContext'
 import { commonMessages } from '../../helpers/translation'
+
+let isBtnSysCwlLoginSetted = false
 
 const AuthButton: React.VFC = () => {
   const { formatMessage } = useIntl()
@@ -31,6 +33,21 @@ const AuthButton: React.VFC = () => {
       setVisible && setVisible(true)
     }
   }
+
+  useEffect(() => {
+    function getHomePageLoginButton() {
+      const btn_sys_cwl_login_other_using = document.querySelector('.btn_sys_cwl_login_other_using')
+      return btn_sys_cwl_login_other_using
+    }
+    // 首頁登入前「免費加入天下學習」按鈕連結
+    setTimeout(() => {
+      const homePageLoginButton = getHomePageLoginButton()
+      if (homePageLoginButton && !isBtnSysCwlLoginSetted) {
+        isBtnSysCwlLoginSetted = true
+        homePageLoginButton.addEventListener('click', handleClick)
+      }
+    }, 500)
+  })
 
   return (
     <>
