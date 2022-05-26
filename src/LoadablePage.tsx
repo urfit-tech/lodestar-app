77a3507908d8ebbdd40df2e38d4cc41cbcca5904
Logan <clothe09986@gmail.com>
2022-05-27 07:34:36 +0800

    [FEATURE] refetchNotification move to LoadablePage

--- src/LoadablePage.tsx
+++ src/LoadablePage.tsx
@@ -1,17 +1,23 @@
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 import { useTracking } from 'lodestar-app-element/src/hooks/tracking'
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import ReactPixel from 'react-facebook-pixel'
 import ReactGA from 'react-ga'
+import NotificationContext from './contexts/NotificationContext'
 
 const LoadablePage: React.VFC<{ pageName: string }> = ({ pageName }) => {
   const tracking = useTracking()
   const { settings } = useApp()
+  const { refetchNotifications } = useContext(NotificationContext)
 
   useEffect(() => {
     settings['tracking.ga_id'] && ReactGA.pageview(window.location.pathname + window.location.search)
     settings['tracking.fb_pixel_id'] && ReactPixel.pageView()
   }, [settings, tracking])
+
+  useEffect(() => {
+    refetchNotifications && refetchNotifications()
+  }, [pageName, refetchNotifications])
 
   const PageComponent = React.lazy(() => import(`./pages/${pageName}`))
   return <PageComponent />
