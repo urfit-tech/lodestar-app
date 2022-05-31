ce4d885d8f38182eb800428f5e5e3b79e84fd0e8
logan <clothe09986@gmail.com>
2022-05-31 11:36:01 +0800

    [FEATURE] adjust SocialSharePopover UI

--- src/components/common/SocialSharePopover.tsx
+++ src/components/common/SocialSharePopover.tsx
@@ -1,20 +1,25 @@
 import { Icon } from '@chakra-ui/icons'
+import { IconButton } from '@chakra-ui/react'
 import { Popover } from 'antd'
 import { useState } from 'react'
 import { AiOutlineLink } from 'react-icons/ai'
 import { BsShareFill } from 'react-icons/bs'
-import {
-  FacebookShareButton,
-  FacebookIcon,
-  RedditShareButton,
-  RedditIcon,
-  TelegramShareButton,
-  TelegramIcon,
-  TwitterShareButton,
-  TwitterIcon,
-  LineShareButton,
-  LineIcon,
-} from 'react-share'
+import { FacebookIcon, FacebookShareButton, LineIcon, LineShareButton } from 'react-share'
+import styled from 'styled-components'
+
+const StyledIconButton = styled(IconButton)`
+  border: 1px solid var(--gray-light);
+  border-radius: 50% !important;
+  background: white;
+`
+
+const StyledPopperContent = styled.div`
+  padding: 11px 12px;
+  display: grid;
+  place-items: center;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 10px;
+`
 
 const SocialSharePopover: React.VFC<{ url: string }> = ({ url }) => {
   const [visible, setVisible] = useState(false)
@@ -26,48 +31,48 @@ const SocialSharePopover: React.VFC<{ url: string }> = ({ url }) => {
     url,
   }
   const handleVisibleOnChange = async (newVisible: boolean) => {
-    if (window.navigator.share) {
-      try {
-        await window.navigator.share(shareData)
-      } catch (e) {
-        console.error(e)
+    if (window.navigator.userAgent) {
+      const isMobile: boolean = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        window.navigator.userAgent,
+      )
+      if (isMobile) {
+        try {
+          await window.navigator.share(shareData)
+        } catch (e) {
+          console.error(e)
+        }
+      } else {
+        setVisible(newVisible)
       }
-    } else {
-      setVisible(newVisible)
+    }
+  }
+  const handleCopyLink = () => {
+    if (window.navigator.clipboard) {
+      window.navigator.clipboard.writeText(url)
     }
   }
   const ShareButtons = (
-    <>
-      <Icon as={AiOutlineLink} boxSize={size + 'px'} />
+    <StyledPopperContent>
       <FacebookShareButton url={url}>
         <FacebookIcon size={size} round={round} />
       </FacebookShareButton>
       <LineShareButton url={url}>
         <LineIcon size={size} round={round} />
       </LineShareButton>
-      <RedditShareButton url={url}>
-        <RedditIcon size={size} round={round} />
-      </RedditShareButton>
-      <TelegramShareButton url={url}>
-        <TelegramIcon size={size} round={round} />
-      </TelegramShareButton>
-      <TwitterShareButton url={url}>
-        <TwitterIcon size={size} round={round} />
-      </TwitterShareButton>
-    </>
+      <Icon as={AiOutlineLink} boxSize={size + 'px'} onClick={handleCopyLink} className="cursor-pointer" />
+    </StyledPopperContent>
   )
+
   return (
-    <>
-      <Popover
-        content={ShareButtons}
-        placement="bottomLeft"
-        trigger="click"
-        visible={visible}
-        onVisibleChange={handleVisibleOnChange}
-      >
-        <Icon as={BsShareFill} style={{ cursor: 'pointer' }} />
-      </Popover>
-    </>
+    <Popover
+      content={ShareButtons}
+      placement="bottom"
+      trigger="click"
+      visible={visible}
+      onVisibleChange={handleVisibleOnChange}
+    >
+      <StyledIconButton variant="ghost" icon={<Icon as={BsShareFill} color="#4c5b8f" />} className="mr-2" />
+    </Popover>
   )
 }
 export default SocialSharePopover
