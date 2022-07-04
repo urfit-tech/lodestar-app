4442541bf2ba4b56ac6f28dbf22e6fa1e88eb6e1
sunnylochun <lo@qraft.app>
2022-07-04 13:41:56 +0800

    [REFACTOR] member certificate

--- src/pages/member/CertificateCollectionAdminPage.tsx
+++ src/pages/member/CertificateCollectionAdminPage.tsx
@@ -1,21 +1,25 @@
 import { SkeletonText } from '@chakra-ui/react'
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
+import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
 import React from 'react'
 import { useIntl } from 'react-intl'
 import CertificateCard from '../../components/common/CertificateCard'
 import DefaultLayout from '../../components/layout/DefaultLayout'
 import MemberAdminLayout from '../../components/layout/MemberAdminLayout'
 import { commonMessages } from '../../helpers/translation'
-import { useCertificateColleaction } from '../../hooks/certificate'
+import { useMemberCertificateCollection } from '../../hooks/certificate'
 import { ReactComponent as MemberCertificateIcon } from '../../images/certificate.svg'
 import ForbiddenPage from '../ForbiddenPage'
 
 const CertificatesCollectionAdminPage: React.VFC = () => {
   const { formatMessage } = useIntl()
   const app = useApp()
-  const { certificates } = useCertificateColleaction()
+  const { currentMemberId } = useAuth()
+  const { data: memberCertificates, loading: memberCertificatesLoading } = useMemberCertificateCollection(
+    currentMemberId || '',
+  )
 
-  if (app.loading) {
+  if (app.loading || memberCertificatesLoading) {
     return (
       <DefaultLayout>
         <SkeletonText mt="1" noOfLines={4} spacing="4" />
@@ -32,9 +36,9 @@ const CertificatesCollectionAdminPage: React.VFC = () => {
       content={{ icon: MemberCertificateIcon, title: formatMessage(commonMessages.content.certificate) }}
     >
       <div className="row">
-        {certificates.map(certificate => (
-          <div className="col-12 col-xl-6" key={certificate.id}>
-            <CertificateCard certificate={certificate} />
+        {memberCertificates.map(memberCertificate => (
+          <div className="col-12 col-xl-6" key={memberCertificate.id}>
+            <CertificateCard certificate={memberCertificate.certificate} memberCertificate={memberCertificate} />
           </div>
         ))}
       </div>
