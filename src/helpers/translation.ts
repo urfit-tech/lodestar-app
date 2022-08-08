dcc6c8c8d22ae1a54810a2184dfc6f7fe629074a
Logan <clothe09986@gmail.com>
2022-08-08 18:19:35 +0800

    [FEATURE] adjust alert isEnrolled text`

--- src/helpers/translation.ts
+++ src/helpers/translation.ts
@@ -139,7 +139,7 @@ export const commonMessages = {
   }),
   alert: defineMessages({
     noPath: { id: 'common.alert.noPath', defaultMessage: '無此路徑' },
-    isEquity: { id: 'common.alert.isEquity', defaultMessage: '已擁有此課程，確定兌換?' },
+    isEnrolled: { id: 'common.alert.isEnrolled', defaultMessage: '你已持有課堂課程，確定要換購嗎？' },
   }),
   button: defineMessages({
     reset: { id: 'common.button.reset', defaultMessage: '重置' },
