c99984d4801358e396dd9f33134fbcd711e29a4a
emily40830 <emily40830@gmail.com>
2022-11-26 23:03:30 +0800

    feat: add page details

--- src/pages/member/DeviceManagementAdminPage.tsx
+++ src/pages/member/DeviceManagementAdminPage.tsx
@@ -1,16 +1,18 @@
 import { Icon, SkeletonText } from '@chakra-ui/react'
-import { Button } from 'antd'
+import { Button, message, Modal } from 'antd'
+import { CommonTitleMixin } from 'lodestar-app-element/src/components/common'
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
-import React, { useEffect } from 'react'
-import { useIntl } from 'react-intl'
+import React, { useEffect, useState } from 'react'
+import { defineMessages, useIntl } from 'react-intl'
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 import AdminCard from '../../components/common/AdminCard'
+import { BREAK_POINT } from '../../components/common/Responsive'
 import DefaultLayout from '../../components/layout/DefaultLayout'
 import MemberAdminLayout from '../../components/layout/MemberAdminLayout'
 import { commonMessages } from '../../helpers/translation'
-import { DeviceIcon } from '../../images'
+import { DeviceIcon, ComputerIcon, MobileIcon, TabletIcon } from '../../images'
 import ForbiddenPage from '../ForbiddenPage'
 
 const deviceCollection = [
@@ -44,6 +46,25 @@ const deviceCollection = [
   },
 ]
 
+const messages = defineMessages({
+  description: {
+    id: 'page.deviceManagementAdmin.description',
+    defaultMessage: '帳號至多綁定 {number} 個裝置，如欲解除裝置請點選「退出」進行解除綁定與終止連線。',
+  },
+  lastLoginAt: { id: 'page.deviceManagementAdmin.lastLoginAt', defaultMessage: '上次登入 : {dateTime}' },
+  browser: { id: 'page.deviceManagementAdmin.browser', defaultMessage: '瀏覽器 : {browser}' },
+  ipAddress: { id: 'page.deviceManagementAdmin.ipAddress', defaultMessage: 'IP 位址 : {ipAddress}' },
+  logout: { id: 'page.deviceManagementAdmin.logout', defaultMessage: '退出' },
+  desktop: { id: 'page.deviceManagementAdmin.desktop', defaultMessage: '電腦 {os}' },
+  tablet: { id: 'page.deviceManagementAdmin.tablet', defaultMessage: '平板 {os}' },
+  mobile: { id: 'page.deviceManagementAdmin.mobile', defaultMessage: '手機 {os}' },
+  unKnownDevice: { id: 'page.deviceManagementAdmin.unKnownDevice', defaultMessage: '未知的裝置 {os}' },
+  logoutTitle: {id: 'page.deviceManagementAdmin.logoutTitle', defaultMessage: '退出裝置' },
+  logoutDescription: { id: 'page.deviceManagementAdmin.logoutDescription', defaultMessage: '若進行退出裝置將會解除此裝置的綁定與終止連線。'},
+  logoutCancel: { id: 'page.deviceManagementAdmin.logoutCancel', defaultMessage: '取消'},
+  logoutConfirm: { id: 'page.deviceManagementAdmin.logoutConfirm', defaultMessage: '確認'}
+})
+
 const StyledDescription = styled.span`
   color: var(--gray-darker);
   font-size: 16px;
@@ -51,8 +72,64 @@ const StyledDescription = styled.span`
 `
 
 const StyledItem = styled.div`
-  padding: 0.75rem 0;
-  border-bottom: 1px solid #efefef;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  padding: 0.75rem 1rem;
+  border-bottom: 2px solid var(--gray-light);
+
+  @media (min-width: ${BREAK_POINT}px) {
+    min-height: 145px;
+    flex-flow: row;
+    justify-content: space-between;
+  }
+`
+
+const StyledDeviceDetailsSection = styled.div`
+  display: flex;
+  flex-flow: column;
+  margin-bottom: 1rem;
+  align-items: center;
+
+  @media (min-width: ${BREAK_POINT}px) {
+    flex-flow: row;
+    margin-bottom: 0rem;
+    > div:first-child{
+      min-width: 13rem;
+    }
+  }
+`
+
+const StyledDeviceDisplayContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+
+  margin-right: 0rem;
+  margin-bottom: 1rem;
+  > svg {
+    margin-right: 0rem;
+  }
+
+  @media (min-width: ${BREAK_POINT}px) {
+    flex-flow: row;
+    margin-right: 1.5rem;
+    margin-bottom: 0rem;
+
+    > svg {
+      margin-right: 1.5rem;
+    }
+  }
+`
+
+const StyledLogoutSection = styled.div`
+  margin-left: 0rem;
+  margin-bottom: 1rem;
+
+  @media (min-width: ${BREAK_POINT}px) {
+    margin-left: 3rem;
+    margin-bottom: 0rem;
+  }
 `
 
 const StyledDeviceTitle = styled.span`
@@ -75,11 +152,68 @@ const StyledLoginInfo = styled.div`
   color: var(--gray-darker);
 `
 
+
+const StyledModal = styled(Modal)`
+  && .ant-modal-footer {
+    padding: 0 1.5rem 1.5rem;
+  }
+  && .ant-modal-footer {
+    border-top: 0;
+    padding: 0 1.5rem 1.5rem;
+  }
+`
+
+const StyledModalTitle = styled.div`
+  ${CommonTitleMixin}
+`
+
+const DeviceDisplaySection: React.VFC<{ type: string; os: string }> = ({ type, os }) => {
+  const { formatMessage } = useIntl()
+  if (type === 'desktop') {
+    return (
+      <>
+        <Icon as={ComputerIcon} w={73} h={73} />
+        <StyledDeviceTitle>{formatMessage(messages.desktop, { os })}</StyledDeviceTitle>
+      </>
+    )
+  }
+  if (type === 'mobile') {
+    return (
+      <>
+        <Icon as={MobileIcon} w={73} h={73} />
+        <StyledDeviceTitle>{formatMessage(messages.mobile, { os })}</StyledDeviceTitle>
+      </>
+    )
+  }
+  if (type === 'tablet') {
+    return (
+      <>
+        <Icon as={TabletIcon} w={73} h={73} />
+        <StyledDeviceTitle>{formatMessage(messages.tablet, { os })}</StyledDeviceTitle>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <Icon as={DeviceIcon} w={73} h={73} />
+      <StyledDeviceTitle>{formatMessage(messages.unKnownDevice) + os}</StyledDeviceTitle>
+    </>
+  )
+}
+
 const DeviceManagementAdminPage: React.VFC = () => {
   const { formatMessage } = useIntl()
   const history = useHistory()
   const { currentMemberId, isAuthenticated, isAuthenticating } = useAuth()
   const { settings, loading, enabledModules } = useApp()
+
+  const [ modalVisible , setModalVisible ] = useState(false)
+  const [logoutLoading, setLogoutLoading ] = useState(false)
+
+  const handleLogout = ()=>{
+    
+  }
 
   useEffect(() => {
     if (!isAuthenticating && !isAuthenticated && !currentMemberId) {
@@ -95,46 +229,67 @@ const DeviceManagementAdminPage: React.VFC = () => {
     )
   }
 
-  // if (!loading && !enabledModules.device_management) {
-  //   return <ForbiddenPage />
-  // }
+  if (!loading && !enabledModules.device_management) {
+    return <ForbiddenPage />
+  }
 
   return (
     <MemberAdminLayout content={{ icon: DeviceIcon, title: formatMessage(commonMessages.content.deviceManagement) }}>
       <div className="mb-5">
         <StyledDescription>
-          {formatMessage(
-            {
-              id: 'common.deviceManagementAdmin.description',
-              defaultMessage: '帳號至多綁定 {number} 個裝置，如欲解除裝置請點選「退出」進行解除綁定與終止連線。',
-            },
-            {
-              number: settings['device_num'] || 1,
-            },
-          )}
+          {formatMessage(messages.description, {
+            number: settings['device_num'] || 1,
+          })}
         </StyledDescription>
       </div>
 
       <AdminCard style={{ padding: '0px 16px' }}>
         {deviceCollection.map(device => {
           return (
-            <StyledItem className="d-flex align-items-center">
-              <div className="mr-5">
-                <Icon as={DeviceIcon} className="my-auto mr-3" />
-                <StyledDeviceTitle>{device.os}</StyledDeviceTitle>
-              </div>
-              <div className="flex-lg-grow-1">
-                <StyledLoginInfo>上次登入：{device.lastLoginAt}</StyledLoginInfo>
-                <StyledLoginInfo>瀏覽器：{device.browser}</StyledLoginInfo>
-                <StyledLoginInfo>IP 位址：{device.ipAddress}</StyledLoginInfo>
-              </div>
-              <div className="">
-                <Button>退出</Button>
-              </div>
+            <StyledItem>
+              <StyledDeviceDetailsSection>
+                <StyledDeviceDisplayContainer>
+                  <DeviceDisplaySection type={device.type} os={device.os} />
+                </StyledDeviceDisplayContainer>
+                <div>
+                  <StyledLoginInfo>
+                    {formatMessage(messages.lastLoginAt, { dateTime: device.lastLoginAt })}
+                  </StyledLoginInfo>
+                  <StyledLoginInfo>{formatMessage(messages.browser, { browser: device.browser })}</StyledLoginInfo>
+                  <StyledLoginInfo>
+                    {formatMessage(messages.ipAddress, { ipAddress: device.ipAddress })}
+                  </StyledLoginInfo>
+                </div>
+              </StyledDeviceDetailsSection>
+              <StyledLogoutSection>
+                <Button onClick={()=>setModalVisible(true)}>{formatMessage(messages.logout)}</Button>
+              </StyledLogoutSection>
             </StyledItem>
           )
         })}
       </AdminCard>
+
+
+      <StyledModal
+            width={400}
+            centered
+            visible={modalVisible}
+            okText={formatMessage(messages.logoutConfirm)}
+            cancelText={formatMessage(messages.logoutCancel)}
+            okButtonProps={{ loading: logoutLoading, type: 'danger' }}
+            onOk={() => handleLogout()}
+            onCancel={() => {
+              setModalVisible(false)
+              setLogoutLoading(false)
+            }}
+          >
+            <StyledModalTitle className="mb-4">
+              {formatMessage(messages.logoutTitle)}
+            </StyledModalTitle>
+            <div className="mb-4">
+              {formatMessage(messages.logoutDescription)}
+            </div>
+          </StyledModal>
     </MemberAdminLayout>
   )
 }
