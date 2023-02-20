77565f7330579ea5c2791ad4620d3ff5df9732f8
ponchimeow <trancetoradora@gmail.com>
2023-02-20 23:38:07 +0800

    feat: SignupPropertyModal

---
+++ src/components/common/SignupPropertyModal.tsx
@@ -0,0 +1,41 @@
+import { Modal } from 'antd'
+import { CommonLargeTitleMixin } from 'lodestar-app-element/src/components/common'
+import { useAuth } from 'lodestar-app-element/src/contexts/AuthContext'
+import { useEffect, useState } from 'react'
+import { useIntl } from 'react-intl'
+import { useLocation } from 'react-router-dom'
+import styled from 'styled-components'
+import SignupPropertySection from './SignupPropertySection'
+import commonMessages from './translation'
+
+export const StyledTitle = styled.h1`
+  ${CommonLargeTitleMixin}
+  margin-bottom: 1.5rem;
+  text-align: center;
+`
+
+const SignupPropertyModal: React.VFC = () => {
+  const { isAuthenticating, currentMemberId, isFinishedSignUpProperty } = useAuth()
+  const { formatMessage } = useIntl()
+  const [visible, setVisible] = useState(false)
+  const location = useLocation()
+
+  useEffect(() => {
+    !isAuthenticating && currentMemberId && !isFinishedSignUpProperty && setVisible(true)
+  }, [location, isAuthenticating, currentMemberId, isFinishedSignUpProperty])
+
+  return (
+    <Modal
+      // closable={false}
+      footer={null}
+      onCancel={() => setVisible && setVisible(false)}
+      visible={visible}
+      maskClosable={false}
+    >
+      <StyledTitle>{formatMessage(commonMessages.SignupPropertyModal.signupInfo)}</StyledTitle>
+      <SignupPropertySection onModalVisible={setVisible} />
+    </Modal>
+  )
+}
+
+export default SignupPropertyModal
