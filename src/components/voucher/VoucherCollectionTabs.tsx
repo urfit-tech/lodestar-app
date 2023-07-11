12d7af2b720988b048dfd3e613fe4ce6026bed62
loischen68 <a8283626@gmail.com>
2023-07-11 23:31:01 +0800

    fix: map using key prop, isSuccess to lowercase

--- src/components/voucher/VoucherCollectionTabs.tsx
+++ src/components/voucher/VoucherCollectionTabs.tsx
@@ -1,12 +1,11 @@
 import { Tab, TabPanels, Tabs } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React from 'react'
 import { useIntl } from 'react-intl'
 import { usersMessages } from '../../helpers/translation'
 import { StyledTabList, StyledTabPanel } from '../../pages/GroupBuyingCollectionPage'
 import Voucher, { VoucherProps } from './Voucher'
 
 const VoucherCollectionTabs: React.VFC<{ vouchers: VoucherProps[] }> = ({ vouchers }) => {
-  const [activeKey, setActiveKey] = useState('available')
   const { formatMessage } = useIntl()
 
   const tabContents: {
@@ -41,15 +40,13 @@ const VoucherCollectionTabs: React.VFC<{ vouchers: VoucherProps[] }> = ({ vouche
     <Tabs colorScheme="primary">
       <StyledTabList>
         {tabContents.map(v => (
-          <Tab key={v.key} onClick={() => setActiveKey(v.key)} isSelected={v.key === activeKey}>
-            {v.name}
-          </Tab>
+          <Tab key={v.key}>{v.name}</Tab>
         ))}
       </StyledTabList>
 
       <TabPanels>
         {tabContents.map(v => (
-          <StyledTabPanel className="row">
+          <StyledTabPanel className="row" key={v.key}>
             {vouchers.filter(v.isDisplay).map(voucher => (
               <div key={voucher.id} className="col-12 col-lg-6">
                 <Voucher {...voucher} />
