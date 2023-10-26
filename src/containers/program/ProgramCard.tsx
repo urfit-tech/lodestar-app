4785f1e2bfb6ebfcd0ba102470b51a7b019c75d4
Hanz <e102777tw@gmail.com>
2023-10-26 10:35:47 +0800

    Revert "Revert "Merge pull request #433 from urfit-tech/feat/member-page-program-block-adjust""
    This reverts commit c5c618cb538180e51a58dcd122c61ca96515f0a2.

--- src/containers/program/ProgramCard.tsx
+++ src/containers/program/ProgramCard.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from '@apollo/client'
-import { Box } from '@chakra-ui/react'
+import { Box, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
+import dayjs from 'dayjs'
 import { CommonTitleMixin, MultiLineTruncationMixin } from 'lodestar-app-element/src/components/common/index'
 import { sum } from 'ramda'
 import React from 'react'
@@ -13,30 +14,64 @@ import hasura from '../../hasura'
 import EmptyCover from '../../images/empty-cover.png'
 import { ProgramPreview, ProgramRoleName } from '../../types/program'
 
-const StyledWrapper = styled.div`
+const StyledWrapper = styled.div<{ view?: string }>`
+  ${props =>
+    props.view === 'List' &&
+    `
+    display:flex;
+    align-items:center;
+  `}
   overflow: hidden;
   background-color: white;
   border-radius: 4px;
   box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.06);
 `
-const StyledMeta = styled.div`
-  padding: 1.25rem;
+const StyledMeta = styled.div<{ view?: string }>`
+  ${props =>
+    props.view === 'List'
+      ? `
+      width:80%;
+      display:flex;
+      justify-content: space-between;
+      align-items:center;
+    `
+      : `padding: 1.25rem;`}
 `
-const StyledTitle = styled.div`
+const StyledTitle = styled.div<{ view?: string }>`
   ${MultiLineTruncationMixin}
   ${CommonTitleMixin}
-  margin-bottom: 1.25rem;
-  height: 3em;
+  ${props =>
+    props.view === 'List'
+      ? `
+      margin-bottom: 0.25rem;
+      display:block;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      `
+      : `
+      margin-bottom: 1.25rem;
+      height: 3rem;
+      `}
 `
-const StyledDescription = styled.div`
+const StyledDescription = styled.div<{ size?: string; view?: string }>`
+  ${props =>
+    props.size === 'small'
+      ? `
+      font-size: 12px;
+      `
+      : `
+      font-size: 14px;
+      height: 3em;
+      margin-bottom: 1.25rem;
+      `}
+  ${props => (props.view === 'List' ? '' : 'margin-bottom: 0.5rem;')}
   ${MultiLineTruncationMixin}
-  margin-bottom: 1.25rem;
-  height: 3em;
   color: var(--gray-dark);
-  font-size: 14px;
   letter-spacing: 0.4px;
 `
-const AvatarPlaceHolder = styled.div`
+const AvatarPlaceHolder = styled.div<{ view?: string }>`
+  ${props => props.view === 'List' && 'width: 40%;'}
   height: 2rem;
 `
 
@@ -49,69 +84,190 @@ const ProgramCard: React.VFC<{
   withProgress?: boolean
   isExpired?: boolean
   previousPage?: string
-}> = ({ memberId, programId, programType, noInstructor, noPrice, withProgress, isExpired, previousPage }) => {
-  const { programPreview } = useProgramPreview(programId)
+  view?: string
+  programDatetimeEnabled?: boolean
+  programDeliveredAt?: Date
+}> = ({
+  memberId,
+  programId,
+  programType,
+  noInstructor,
+  noPrice,
+  withProgress,
+  isExpired,
+  previousPage,
+  view,
+  programDatetimeEnabled,
+  programDeliveredAt,
+}) => {
+  const { loadingProgramPreview, programPreview } = useProgramPreview(programId)
   const { loadingProgress, programContentProgress } = useProgramContentProgress(programId, memberId)
 
   const viewRate = programContentProgress?.length
     ? sum(programContentProgress.map(contentProgress => contentProgress.progress)) / programContentProgress.length
     : 0
 
+  const lastViewDate = programContentProgress?.length
+    ? programContentProgress
+        .map(contentProgress => contentProgress.updatedAt)
+        .sort((a, b) => {
+          if (a && b) {
+            return +new Date(b) - +new Date(a)
+          }
+          return 0
+        })[0]
+    : undefined
+
   return (
-    <Box opacity={isExpired ? '50%' : '100%'}>
-      {!noInstructor && programPreview?.roles && (
-        <AvatarPlaceHolder className="my-3">
-          {programPreview.roles
-            .filter(role => role.name === 'instructor')
-            .slice(0, 1)
-            .map(role => (
-              <MemberAvatar key={role.memberId} memberId={role.memberId} withName />
-            ))}
-        </AvatarPlaceHolder>
-      )}
+    <>
+      {view === 'Grid' && (
+        <Box opacity={isExpired ? '50%' : '100%'}>
+          {loadingProgramPreview ? (
+            <AvatarPlaceHolder className="my-3">
+              <SkeletonCircle size="10" />
+            </AvatarPlaceHolder>
+          ) : (
+            !noInstructor && (
+              <AvatarPlaceHolder className="my-3">
+                {programPreview?.roles
+                  .filter(role => role.name === 'instructor')
+                  .slice(0, 1)
+                  .map(role => (
+                    <MemberAvatar key={role.memberId} memberId={role.memberId} withName />
+                  ))}
+              </AvatarPlaceHolder>
+            )
+          )}
 
-      <Link
-        to={
-          isExpired
-            ? `/programs/${programId}?visitIntro=1`
-            : programType && previousPage
-            ? `/programs/${programId}?type=${programType}&back=${previousPage}`
-            : programType
-            ? `/programs/${programId}?type=${programType}`
-            : previousPage
-            ? `/programs/${programId}?back=${previousPage}`
-            : `/programs/${programId}`
-        }
-      >
-        <StyledWrapper>
-          <CustomRatioImage
-            width="100%"
-            ratio={9 / 16}
-            src={
-              (programPreview &&
-                (programPreview.coverThumbnailUrl || programPreview.coverUrl || programPreview.coverMobileUrl)) ||
-              EmptyCover
+          <Link
+            to={
+              isExpired
+                ? `/programs/${programId}?visitIntro=1`
+                : programType && previousPage
+                ? `/programs/${programId}?type=${programType}&back=${previousPage}`
+                : programType
+                ? `/programs/${programId}?type=${programType}`
+                : previousPage
+                ? `/programs/${programId}?back=${previousPage}`
+                : `/programs/${programId}`
             }
-            shape="rounded"
-          />
-          <StyledMeta>
-            <StyledTitle>{programPreview && programPreview.title}</StyledTitle>
-            <StyledDescription>{programPreview && programPreview.abstract}</StyledDescription>
-
-            {withProgress && !loadingProgress && <ProgressBar percent={Math.floor(viewRate * 100)} />}
-          </StyledMeta>
-        </StyledWrapper>
-      </Link>
-    </Box>
+          >
+            <StyledWrapper>
+              <CustomRatioImage
+                width="100%"
+                ratio={9 / 16}
+                src={
+                  (programPreview &&
+                    (programPreview.coverThumbnailUrl || programPreview.coverUrl || programPreview.coverMobileUrl)) ||
+                  EmptyCover
+                }
+                shape="rounded"
+              />
+              <StyledMeta>
+                {loadingProgramPreview ? (
+                  <SkeletonText m="4" noOfLines={4} spacing="4" skeletonHeight="2" />
+                ) : (
+                  <>
+                    <StyledTitle>{programPreview?.title}</StyledTitle>
+                    {programDatetimeEnabled && (
+                      <StyledDescription size="small">
+                        {`${dayjs(programDeliveredAt).format('YYYY-MM-DD')} 購買`}
+                        {lastViewDate ? ` / ${dayjs(lastViewDate).format('YYYY-MM-DD')}上次觀看` : ' / 尚未觀看'}
+                      </StyledDescription>
+                    )}
+                    <StyledDescription>{programPreview?.abstract}</StyledDescription>
+                  </>
+                )}
+                {withProgress && (
+                  <ProgressBar percent={Math.floor(viewRate * 100)} width="100%" loading={loadingProgress} />
+                )}
+              </StyledMeta>
+            </StyledWrapper>
+          </Link>
+        </Box>
+      )}
+      {view === 'List' && (
+        <Box opacity={isExpired ? '50%' : '100%'} width="100%">
+          <Link
+            to={
+              isExpired
+                ? `/programs/${programId}?visitIntro=1`
+                : programType && previousPage
+                ? `/programs/${programId}?type=${programType}&back=${previousPage}`
+                : programType
+                ? `/programs/${programId}?type=${programType}`
+                : previousPage
+                ? `/programs/${programId}?back=${previousPage}`
+                : `/programs/${programId}`
+            }
+          >
+            <StyledWrapper view={view}>
+              <CustomRatioImage
+                width="15%"
+                height="15%"
+                margin="12px"
+                ratio={9 / 16}
+                src={
+                  (programPreview &&
+                    (programPreview.coverThumbnailUrl || programPreview.coverUrl || programPreview.coverMobileUrl)) ||
+                  EmptyCover
+                }
+                shape="rounded"
+              />
+              <StyledMeta view={view}>
+                <Box minWidth="50%" maxWidth="50%">
+                  {loadingProgramPreview ? (
+                    <SkeletonText m="4" noOfLines={3} spacing="4" skeletonHeight="2" />
+                  ) : (
+                    <>
+                      <StyledTitle view={view}>{programPreview?.title}</StyledTitle>
+                      {programDatetimeEnabled && (
+                        <StyledDescription size="small" view={view}>
+                          {`${dayjs(programDeliveredAt).format('YYYY-MM-DD')} 購買`}
+                          {lastViewDate ? ` / ${dayjs(lastViewDate).format('YYYY-MM-DD')}上次觀看` : ' / 尚未觀看'}
+                        </StyledDescription>
+                      )}
+                    </>
+                  )}
+                </Box>
+                <Box width="100%" display="flex" alignItems="center" justifyContent="flex-end">
+                  {loadingProgramPreview ? (
+                    <AvatarPlaceHolder className="my-3" view={view}>
+                      <SkeletonCircle size="10" />
+                    </AvatarPlaceHolder>
+                  ) : (
+                    <>
+                      {!noInstructor && (
+                        <AvatarPlaceHolder className="my-3" view={view}>
+                          {programPreview?.roles
+                            .filter(role => role.name === 'instructor')
+                            .slice(0, 1)
+                            .map(role => (
+                              <MemberAvatar key={role.memberId} memberId={role.memberId} withName view={view} />
+                            ))}
+                        </AvatarPlaceHolder>
+                      )}
+                    </>
+                  )}
+                  {withProgress && (
+                    <ProgressBar percent={Math.floor(viewRate * 100)} width="40%" loading={loadingProgress} />
+                  )}
+                </Box>
+              </StyledMeta>
+            </StyledWrapper>
+          </Link>
+        </Box>
+      )}
+    </>
   )
 }
 
 export default ProgramCard
 
 const useProgramPreview = (programId: string) => {
-  const { loading, data, error, refetch } = useQuery<hasura.GET_PROGRAM_PREVIEW, hasura.GET_PROGRAM_PREVIEWVariables>(
+  const { loading, data, error, refetch } = useQuery<hasura.GetProgramPreview, hasura.GetProgramPreviewVariables>(
     gql`
-      query GET_PROGRAM_PREVIEW($programId: uuid!) {
+      query GetProgramPreview($programId: uuid!) {
         program_by_pk(id: $programId) {
           id
           cover_url
