82977d6572564df6c801aa78a443d0635abc6e48
Ponchimeow <trancetoradora@gmail.com>
2024-08-16 09:55:51 +0800

    Merge pull request #578 from urfit-tech/feat/cart-display-issue
    Feat/cart display issue
    
    (cherry picked from commit 5003ec27191126a18fe9105aa868b246b787b5c6)

---
+++ src/services/cart/AddCartProductOperator.ts
@@ -0,0 +1,37 @@
+import { ProductType } from '../../types/product'
+import { CartOperator } from './CartOperator'
+import { CartOperatorEnum } from './CartOperatorEnum'
+
+export class AddCartProductOperator extends CartOperator {
+  operator = CartOperatorEnum.ADD_CART_PRODUCT
+
+  async operation(
+    productType: ProductType,
+    productTarget: string,
+    cartDisableSetting: boolean,
+    trackingOptions: {},
+    productOptions?: { [key: string]: any },
+  ) {
+    const cachedCartProducts = this.getLocalCartProducts()
+    const repeatedCartProduct = cachedCartProducts.find(
+      cartProduct => cartProduct.productId === `${productType}_${productTarget}`,
+    )
+    const newCartProducts = Number(cartDisableSetting)
+      ? []
+      : cachedCartProducts.filter(cartProduct => cartProduct.productId !== `${productType}_${productTarget}`)
+    const newCartProduct = {
+      productId: `${productType}_${productTarget}`,
+      shopId: '',
+      options:
+        productType === 'MerchandiseSpec'
+          ? {
+              quantity: (productOptions?.quantity || 1) + (repeatedCartProduct?.options?.quantity || 0),
+              tracking: trackingOptions,
+            }
+          : { ...productOptions, tracking: trackingOptions },
+    }
+    newCartProducts.push(newCartProduct)
+    localStorage.setItem('kolable.cart._products', JSON.stringify(newCartProducts))
+    this.syncCartProducts(this.operator)
+  }
+}
