14bc49cd2bbce166b5e611d24f99b55cc9ab03ac
Logan <clothe09986@gmail.com>
2024-08-29 17:04:03 +0800

    fix: any type

--- src/components/checkout/CheckoutBlock.tsx
+++ src/components/checkout/CheckoutBlock.tsx
@@ -175,7 +175,7 @@ const CheckoutBlock: React.VFC<{
           ...member?.invoice,
         }
     cachedPaymentInfo.payment = cachedPayment
-      ? (JSON.parse(cachedPayment) as any)
+      ? (JSON.parse(cachedPayment) as PaymentProps)
       : cachedPaymentInfo.payment && member?.payment
       ? {
           ...cachedPaymentInfo.payment,
