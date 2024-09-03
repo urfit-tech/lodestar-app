27b58d65ba778a3780e5278fe9b89a000b05b255
Ponchimeow <trancetoradora@gmail.com>
2024-09-03 17:52:05 +0800

    Merge pull request #599 from urfit-tech/refactor/adjust-VoucherExchangeModal
    refactor: adjust VoucherExchangeModal and MessageBox
    (cherry picked from commit 03ee84593b1c3417939f4e24c04b3cafac46e746)

--- src/components/voucher/VoucherExchangeModal.tsx
+++ src/components/voucher/VoucherExchangeModal.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from '@apollo/client'
+import { ApolloError, gql, useQuery } from '@apollo/client'
 import { Box, Button, Divider, Icon, Input, Spinner, Text } from '@chakra-ui/react'
 import { Checkbox, message, Modal } from 'antd'
 import axios from 'axios'
@@ -34,56 +34,49 @@ const StyledNotice = styled.div`
   font-size: 14px;
   letter-spacing: 0.4px;
 `
-const VoucherExchangeModal: React.VFC<{
+
+const VoucherExchangeModalBlock: React.FC<{
+  visible: boolean
+  validProductIds: string[]
+  disabledProductIds: string[]
   voucherId: string
   voucherPlanId: string
-  productQuantityLimit: number
   description: string | null
-  productIds: string[]
-  disabledProductIds: string[]
-  loading: boolean
+  productQuantityLimit: number
+  loadingValidityCheck: boolean
+  error: ApolloError | undefined
   onLoading?: (status: boolean) => void
   onRefetch?: () => void
+  onModalVisibleChange: (status: boolean) => void
 }> = ({
+  visible,
+  validProductIds,
+  disabledProductIds,
   voucherId,
   voucherPlanId,
-  productQuantityLimit,
   description,
-  productIds,
-  disabledProductIds,
-  loading,
+  productQuantityLimit,
+  loadingValidityCheck,
+  error,
   onLoading,
   onRefetch,
+  onModalVisibleChange,
 }) => {
-  const { formatMessage } = useIntl()
-  const history = useHistory()
   const { currentMemberId, authToken } = useAuth()
+  const history = useHistory()
   const { settings } = useApp()
-  const [visible, setVisible] = useState(false)
+  const { loading, productCollection } = useProductCollection(validProductIds)
+  const { formatMessage } = useIntl()
+
   const [messageModalStatus, setMessageModalStatus] = useState<'success' | ''>('')
   const [selectedProductIds, setSelectedProductIds] = useState<string[]>([])
   const [hasPinCode, setHasPinCode] = useState(false)
   const [pinCode, setPinCode] = useState<string | null>(null)
   const [exchanging, setExchanging] = useState(false)
-  const myPageName = settings['nav.my_page.name'] ? settings['nav.my_page.name'] : '我的主頁'
 
-  // check the validity of product which type is ActivityTicket
-  // session ended_at and ticket buy
-  const {
-    loading: loadingValidityCheck,
-    error,
-    data: validActivityTicketIds,
-  } = useQuery<hasura.GET_VALID_ACTIVITY_TICKET>(GET_VALID_ACTIVITY_TICKET, {
-    variables: {
-      activityTicketIds: productIds.filter(v => v.split('_')[0] === 'ActivityTicket').map(v => v.split('_')[1]),
-    },
-  })
-
-  const validProductIds: string[] = productIds.filter(v =>
-    v.split('_')[0] === 'ActivityTicket'
-      ? validActivityTicketIds?.activity_ticket_enrollment_count.find(w => w.activity_ticket_id === v.split('_')[1])
-      : v,
-  )
+  const myPageName = settings['nav.my_page.name']
+    ? settings['nav.my_page.name']
+    : formatMessage(voucherMessages.VoucherExchangeModal.homePage)
 
   const exchangeVoucher = async (selectedProductIds: string[], voucherId: string) => {
     onLoading?.(true)
@@ -114,11 +107,10 @@ const VoucherExchangeModal: React.VFC<{
       })
       .catch(error => handleError(error))
       .finally(() => {
-        onLoading?.(false)
         setExchanging(false)
-        setVisible(false)
         setHasPinCode(false)
         setSelectedProductIds([])
+        onLoading?.(false)
       })
   }
 
@@ -177,21 +169,21 @@ const VoucherExchangeModal: React.VFC<{
       .catch(error => handleError(error))
   }
 
-  const ModalBlock: React.FC = () => {
-    const { loading, productCollection } = useProductCollection(validProductIds)
-
-    return (
-      <Modal
-        width="460px"
-        centered
-        destroyOnClose
-        footer={null}
-        visible={messageModalStatus === 'success'}
-        onCancel={() => {
-          setMessageModalStatus('')
-          onRefetch?.()
-        }}
-      >
+  return (
+    <Modal
+      centered
+      destroyOnClose
+      footer={null}
+      visible={visible}
+      onCancel={() => {
+        onLoading?.(false)
+        setExchanging(false)
+        onModalVisibleChange(false)
+        setSelectedProductIds([])
+        messageModalStatus === 'success' && onRefetch?.()
+      }}
+    >
+      {messageModalStatus === 'success' ? (
         <Box marginY="10px" display={'flex'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
           <MessageBox
             icon={SuccessIcon}
@@ -206,147 +198,175 @@ const VoucherExchangeModal: React.VFC<{
             }
           />
         </Box>
-      </Modal>
+      ) : hasPinCode ? (
+        <>
+          <StyledTitle className="mb-4">
+            {formatMessage(voucherMessages.VoucherExchangeModal.pleaseEnterPinCode)}
+          </StyledTitle>
 
-      <Modal
-        centered
-        destroyOnClose
-        footer={null}
-        visible={visible}
-        onCancel={() => {
-          onLoading?.(false)
-          setExchanging(false)
-          setVisible(false)
-          setSelectedProductIds([])
-        }}
-      >
-        {hasPinCode ? (
-          <>
-            <StyledTitle className="mb-4">
-              {formatMessage(voucherMessages.VoucherExchangeModal.pleaseEnterPinCode)}
-            </StyledTitle>
-
-            <Text className="mb-2">{formatMessage(voucherMessages.VoucherExchangeModal.pinCode)}：</Text>
-            <Input
-              className="mb-3"
-              pinCode={pinCode}
-              onChange={e => setPinCode(e.target.value)}
-              disabled={loading || exchanging}
-            />
-            <div className="text-right">
-              <Button
-                className="mr-2"
-                onClick={() => {
-                  setVisible(false)
-                  setHasPinCode(false)
-                  setSelectedProductIds([])
-                }}
-              >
-                {formatMessage(voucherMessages['*'].cancel)}
-              </Button>
-              <Button
-                colorScheme="primary"
-                isLoading={loading}
-                isDisabled={selectedProductIds.length === 0 || selectedProductIds.length > productQuantityLimit}
-                onClick={() => handleExchangeWithPinCode(voucherPlanId, pinCode)}
-              >
-                {formatMessage(voucherMessages['*'].send)}
-              </Button>
-            </div>
-          </>
-        ) : validProductIds.length === 0 ? (
-          <div className="d-flex flex-column align-items-center p-5">
-            <Icon as={EmptyBoxIcon} className="mb-4" w="90px" h="90px" />
-            <StyledNotice className="d-flex flex-column align-items-center">
-              <div>{formatMessage(voucherMessages.VoucherExchangeModal.emptyNoticeText)}</div>
-              <div>{formatMessage(voucherMessages.VoucherExchangeModal.emptyNoticeText2)}</div>
-            </StyledNotice>
-            <div>
-              <Button
-                w="145px"
-                color="var(--gray-darker)"
-                fontSize="16px"
-                lineHeight="24px"
-                borderRadius="4px"
-                border="solid 1px var(--gray)"
-                variant="outline"
-                onClick={() => {
-                  setVisible(false)
-                  setSelectedProductIds([])
-                }}
-              >
-                {formatMessage(voucherMessages['*'].close)}
-              </Button>
-            </div>
+          <Text className="mb-2">{formatMessage(voucherMessages.VoucherExchangeModal.pinCode)}：</Text>
+          <Input
+            className="mb-3"
+            pinCode={pinCode}
+            onChange={e => setPinCode(e.target.value)}
+            disabled={loading || exchanging}
+          />
+          <div className="text-right">
+            <Button
+              className="mr-2"
+              onClick={() => {
+                onModalVisibleChange(false)
+                setHasPinCode(false)
+                setSelectedProductIds([])
+              }}
+            >
+              {formatMessage(voucherMessages['*'].cancel)}
+            </Button>
+            <Button
+              colorScheme="primary"
+              isLoading={loading}
+              isDisabled={selectedProductIds.length === 0 || selectedProductIds.length > productQuantityLimit}
+              onClick={() => handleExchangeWithPinCode(voucherPlanId, pinCode)}
+            >
+              {formatMessage(voucherMessages['*'].send)}
+            </Button>
           </div>
-        ) : (
-          <>
-            <StyledTitle className="mb-2">
-              {formatMessage(voucherMessages.VoucherExchangeModal.productQuantityLimitText, {
-                productQuantityLimit: productQuantityLimit,
-              })}
-            </StyledTitle>
-            <StyledDescription className="mb-2">{description}</StyledDescription>
-            <StyledNotice>{formatMessage(voucherMessages.VoucherExchangeModal.notice)}</StyledNotice>
+        </>
+      ) : validProductIds.length === 0 ? (
+        <div className="d-flex flex-column align-items-center p-5">
+          <Icon as={EmptyBoxIcon} className="mb-4" w="90px" h="90px" />
+          <StyledNotice className="d-flex flex-column align-items-center">
+            <div>{formatMessage(voucherMessages.VoucherExchangeModal.emptyNoticeText)}</div>
+            <div>{formatMessage(voucherMessages.VoucherExchangeModal.emptyNoticeText2)}</div>
+          </StyledNotice>
+          <div>
+            <Button
+              w="145px"
+              color="var(--gray-darker)"
+              fontSize="16px"
+              lineHeight="24px"
+              borderRadius="4px"
+              border="solid 1px var(--gray)"
+              variant="outline"
+              onClick={() => {
+                onModalVisibleChange(false)
+                setSelectedProductIds([])
+              }}
+            >
+              {formatMessage(voucherMessages['*'].close)}
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <StyledTitle className="mb-2">
+            {formatMessage(voucherMessages.VoucherExchangeModal.productQuantityLimitText, {
+              productQuantityLimit: productQuantityLimit,
+            })}
+          </StyledTitle>
+          <StyledDescription className="mb-2">{description}</StyledDescription>
+          <StyledNotice>{formatMessage(voucherMessages.VoucherExchangeModal.notice)}</StyledNotice>
 
-            {loading || loadingValidityCheck ? (
-              <Spinner />
-            ) : error ? (
-              <>something went wrong.</>
-            ) : (
-              productCollection.map(product => {
-                const productId = `${product.productType}_${product.targetId}`
-                return (
-                  <div key={product.targetId}>
-                    <div className="d-flex align-items-center justify-content-start">
-                      <Checkbox
-                        className="mr-4"
-                        onChange={e => {
-                          if (e.target.checked) {
-                            setSelectedProductIds([...selectedProductIds, productId])
-                          } else {
-                            setSelectedProductIds(selectedProductIds.filter(id => id !== productId))
-                          }
-                        }}
-                        disabled={
-                          disabledProductIds.includes(productId) ||
-                          (!selectedProductIds.includes(productId) && selectedProductIds.length >= productQuantityLimit)
+          {loading || loadingValidityCheck ? (
+            <Spinner />
+          ) : error ? (
+            <>something went wrong.</>
+          ) : (
+            productCollection.map(product => {
+              const productId = `${product.productType}_${product.targetId}`
+              return (
+                <div key={product.targetId}>
+                  <div className="d-flex align-items-center justify-content-start">
+                    <Checkbox
+                      className="mr-4"
+                      onChange={e => {
+                        if (e.target.checked) {
+                          setSelectedProductIds([...selectedProductIds, productId])
+                        } else {
+                          setSelectedProductIds(selectedProductIds.filter(id => id !== productId))
                         }
-                      />
-                      <ProductItem product={product} />
-                    </div>
-
-                    <Divider className="my-4" />
+                      }}
+                      disabled={
+                        disabledProductIds.includes(productId) ||
+                        (!selectedProductIds.includes(productId) && selectedProductIds.length >= productQuantityLimit)
+                      }
+                    />
+                    <ProductItem product={product} />
                   </div>
-                )
-              })
-            )}
 
-            <div className="text-right">
-              <Button
-                className="mr-2"
-                onClick={() => {
-                  setExchanging(false)
-                  setVisible(false)
-                  setSelectedProductIds([])
-                }}
-              >
-                {formatMessage(voucherMessages['*'].cancel)}
-              </Button>
-              <Button
-                colorScheme="primary"
-                isLoading={loading || exchanging || loadingValidityCheck}
-                isDisabled={selectedProductIds.length === 0 || selectedProductIds.length > productQuantityLimit}
-                onClick={() => handleExchange(voucherPlanId)}
-              >
-                {formatMessage(voucherMessages.VoucherExchangeModal.exchange)}
-              </Button>
-            </div>
-          </>
-        )}
-      </Modal>
-    )
-  }
+                  <Divider className="my-4" />
+                </div>
+              )
+            })
+          )}
+
+          <div className="text-right">
+            <Button
+              className="mr-2"
+              onClick={() => {
+                setExchanging(false)
+                onModalVisibleChange(false)
+                setSelectedProductIds([])
+              }}
+            >
+              {formatMessage(voucherMessages['*'].cancel)}
+            </Button>
+            <Button
+              colorScheme="primary"
+              isLoading={loading || exchanging || loadingValidityCheck}
+              isDisabled={selectedProductIds.length === 0 || selectedProductIds.length > productQuantityLimit}
+              onClick={() => handleExchange(voucherPlanId)}
+            >
+              {formatMessage(voucherMessages.VoucherExchangeModal.exchange)}
+            </Button>
+          </div>
+        </>
+      )}
+    </Modal>
+  )
+}
+
+const VoucherExchangeModal: React.VFC<{
+  voucherId: string
+  voucherPlanId: string
+  productQuantityLimit: number
+  description: string | null
+  productIds: string[]
+  disabledProductIds: string[]
+  loading: boolean
+  onLoading?: (status: boolean) => void
+  onRefetch?: () => void
+}> = ({
+  voucherId,
+  voucherPlanId,
+  productQuantityLimit,
+  description,
+  productIds,
+  disabledProductIds,
+  loading,
+  onLoading,
+  onRefetch,
+}) => {
+  const { formatMessage } = useIntl()
+  const [visible, setVisible] = useState(false)
+
+  // check the validity of product which type is ActivityTicket
+  // session ended_at and ticket buy
+  const {
+    loading: loadingValidityCheck,
+    error,
+    data: validActivityTicketIds,
+  } = useQuery<hasura.GET_VALID_ACTIVITY_TICKET>(GET_VALID_ACTIVITY_TICKET, {
+    variables: {
+      activityTicketIds: productIds.filter(v => v.split('_')[0] === 'ActivityTicket').map(v => v.split('_')[1]),
+    },
+  })
+
+  const validProductIds: string[] = productIds.filter(v =>
+    v.split('_')[0] === 'ActivityTicket'
+      ? validActivityTicketIds?.activity_ticket_enrollment_count.find(w => w.activity_ticket_id === v.split('_')[1])
+      : v,
+  )
 
   return (
     <>
@@ -359,7 +379,20 @@ const VoucherExchangeModal: React.VFC<{
         {formatMessage(voucherMessages.VoucherExchangeModal.useNow)}
       </Button>
 
-      {visible ? <ModalBlock /> : null}
+      <VoucherExchangeModalBlock
+        visible={visible}
+        validProductIds={validProductIds}
+        disabledProductIds={disabledProductIds}
+        loadingValidityCheck={loadingValidityCheck}
+        productQuantityLimit={productQuantityLimit}
+        voucherPlanId={voucherPlanId}
+        voucherId={voucherId}
+        description={description}
+        error={error}
+        onLoading={onLoading}
+        onRefetch={onRefetch}
+        onModalVisibleChange={(visible: boolean) => setVisible(visible)}
+      />
     </>
   )
 }
