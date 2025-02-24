2012b8ad4bfaf27537eaacc865be7b8ba3ea1a7c
Ponchimeow <trancetoradora@gmail.com>
2025-02-24 17:34:52 +0800

    Merge pull request #662 from urfit-tech/fix/login-device-limit
    fix: remove member_device query

--- src/pages/OverLoginDevicePage.tsx
+++ src/pages/OverLoginDevicePage.tsx
@@ -1,16 +1,13 @@
-import { gql, useApolloClient } from '@apollo/client'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 import { useApp } from 'lodestar-app-element/src/contexts/AppContext'
 import { getFingerPrintId } from 'lodestar-app-element/src/hooks/util'
 import OverLoginDeviceModal from '../components/auth/login/OverLoginDeviceModal'
 import DefaultLayout from '../components/layout/DefaultLayout'
-import hasura from '../hasura'
 import { handleError } from '../helpers'
 
 const OverLoginDevicePage: React.FC = () => {
-  const { id: appId, settings } = useApp()
-  const apollo = useApolloClient()
+  const { id: appId } = useApp()
   let member: { id: string; email: string }
   try {
     member = JSON.parse(decodeURIComponent(Cookies.get('member')) || '{}')
@@ -20,38 +17,21 @@ const OverLoginDevicePage: React.FC = () => {
 
   const handleForceLogin = async () => {
     const fingerPrintId = await getFingerPrintId()
-    const { data: memberDevicesData } = await apollo.query<hasura.GetMemberDevices, hasura.GetMemberDevicesVariables>({
-      query: gql`
-        query GetMemberDevices($id: String!) {
-          member_device(where: { member_id: { _eq: $id } }, order_by: { last_login_at: desc }) {
-            member_id
-            fingerprint_id
-            is_login
-            last_login_at
-          }
-        }
-      `,
-      variables: {
-        id: member.id || '',
-      },
-    })
-    if (memberDevicesData.member_device.length < Number(settings['login_device_num'])) {
-      await axios
-        .post(
-          `${process.env.REACT_APP_LODESTAR_SERVER_ENDPOINT}/device/manage-logged-in-limit`,
-          {
-            appId,
-            memberId: member.id,
-            fingerPrintId,
-          },
-          {
-            withCredentials: true,
-          },
-        )
-        .then(() => (window.location.href = '/'))
-        .catch(error => handleError(error))
-        .finally(() => Cookies.remove('member'))
-    }
+    await axios
+      .post(
+        `${process.env.REACT_APP_LODESTAR_SERVER_ENDPOINT}/device/manage-logged-in-limit`,
+        {
+          appId,
+          memberId: member.id,
+          fingerPrintId,
+        },
+        {
+          withCredentials: true,
+        },
+      )
+      .then(() => (window.location.href = '/'))
+      .catch(error => handleError(error))
+      .finally(() => Cookies.remove('member'))
   }
 
   return (
