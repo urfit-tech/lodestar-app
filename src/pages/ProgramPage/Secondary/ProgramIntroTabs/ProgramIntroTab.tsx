7d9b291e32a5ee4fc738faaeb894606dc9260c40
HsuPeiChun <philosophysis@gmail.com>
2025-07-02 16:51:12 +0800

    style: set tab text to primary color in overview, chapter, teacher

--- src/pages/ProgramPage/Secondary/ProgramIntroTabs/ProgramIntroTab.tsx
+++ src/pages/ProgramPage/Secondary/ProgramIntroTabs/ProgramIntroTab.tsx
@@ -1,19 +1,7 @@
 import { Tab } from '@chakra-ui/react'
-import { colors } from '../style'
 
 const ProgramIntroTab: React.FC<{ children: React.ReactElement | string }> = props => {
-  return (
-    <Tab
-      fontWeight={500}
-      color={colors.gray3}
-      _selected={{
-        color: colors.teal,
-        borderColor: colors.teal,
-      }}
-    >
-      {props.children}
-    </Tab>
-  )
+  return <Tab fontWeight={500}>{props.children}</Tab>
 }
 
 export default ProgramIntroTab
